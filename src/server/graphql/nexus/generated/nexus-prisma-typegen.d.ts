434e436b9776ba8e17ce58884474aae880a71901
leedavidcs <lee.david.cs@gmail.com>
2021-06-25 03:23:43 -0700

    Add more models to schema. Generate nexus

--- src/server/graphql/nexus/generated/nexus-prisma-typegen.d.ts
+++ src/server/graphql/nexus/generated/nexus-prisma-typegen.d.ts
@@ -9,23 +9,64 @@ type Pagination = {
 }
 
 // Prisma custom scalar names
-type CustomScalars = 'No custom scalars are used in your Prisma Schema.'
+type CustomScalars = 'DateTime'
 
 // Prisma model type definitions
 interface PrismaModels {
   User: Prisma.User
+  Post: Prisma.Post
+  Comment: Prisma.Comment
+  Skill: Prisma.Skill
 }
 
 // Prisma input types metadata
 interface NexusPrismaInputs {
   Query: {
     users: {
-      filtering: 'AND' | 'OR' | 'NOT' | 'id' | 'email' | 'username' | 'profileUrl' | 'profileImageUrl' | 'provider'
+      filtering: 'AND' | 'OR' | 'NOT' | 'id' | 'email' | 'username' | 'profileUrl' | 'profileImageUrl' | 'provider' | 'posts' | 'comments' | 'skills'
       ordering: 'id' | 'email' | 'username' | 'profileUrl' | 'profileImageUrl' | 'provider'
+    }
+    posts: {
+      filtering: 'AND' | 'OR' | 'NOT' | 'id' | 'author' | 'authorId' | 'title' | 'content' | 'thumbnailImageUrl' | 'comments' | 'createdAt' | 'updatedAt'
+      ordering: 'id' | 'authorId' | 'title' | 'content' | 'thumbnailImageUrl' | 'createdAt' | 'updatedAt'
+    }
+    comments: {
+      filtering: 'AND' | 'OR' | 'NOT' | 'id' | 'author' | 'authorId' | 'post' | 'postId' | 'content' | 'createdAt' | 'updatedAt'
+      ordering: 'id' | 'authorId' | 'postId' | 'content' | 'createdAt' | 'updatedAt'
+    }
+    skills: {
+      filtering: 'AND' | 'OR' | 'NOT' | 'id' | 'name' | 'users'
+      ordering: 'id' | 'name'
     }
   },
   User: {
+    posts: {
+      filtering: 'AND' | 'OR' | 'NOT' | 'id' | 'author' | 'authorId' | 'title' | 'content' | 'thumbnailImageUrl' | 'comments' | 'createdAt' | 'updatedAt'
+      ordering: 'id' | 'authorId' | 'title' | 'content' | 'thumbnailImageUrl' | 'createdAt' | 'updatedAt'
+    }
+    comments: {
+      filtering: 'AND' | 'OR' | 'NOT' | 'id' | 'author' | 'authorId' | 'post' | 'postId' | 'content' | 'createdAt' | 'updatedAt'
+      ordering: 'id' | 'authorId' | 'postId' | 'content' | 'createdAt' | 'updatedAt'
+    }
+    skills: {
+      filtering: 'AND' | 'OR' | 'NOT' | 'id' | 'name' | 'users'
+      ordering: 'id' | 'name'
+    }
+  }
+  Post: {
+    comments: {
+      filtering: 'AND' | 'OR' | 'NOT' | 'id' | 'author' | 'authorId' | 'post' | 'postId' | 'content' | 'createdAt' | 'updatedAt'
+      ordering: 'id' | 'authorId' | 'postId' | 'content' | 'createdAt' | 'updatedAt'
+    }
+  }
+  Comment: {
 
+  }
+  Skill: {
+    users: {
+      filtering: 'AND' | 'OR' | 'NOT' | 'id' | 'email' | 'username' | 'profileUrl' | 'profileImageUrl' | 'provider' | 'posts' | 'comments' | 'skills'
+      ordering: 'id' | 'email' | 'username' | 'profileUrl' | 'profileImageUrl' | 'provider'
+    }
   }
 }
 
@@ -34,6 +75,12 @@ interface NexusPrismaOutputs {
   Query: {
     user: 'User'
     users: 'User'
+    post: 'Post'
+    posts: 'Post'
+    comment: 'Comment'
+    comments: 'Comment'
+    skill: 'Skill'
+    skills: 'Skill'
   },
   Mutation: {
     createOneUser: 'User'
@@ -42,6 +89,24 @@ interface NexusPrismaOutputs {
     deleteOneUser: 'User'
     deleteManyUser: 'AffectedRowsOutput'
     upsertOneUser: 'User'
+    createOnePost: 'Post'
+    updateOnePost: 'Post'
+    updateManyPost: 'AffectedRowsOutput'
+    deleteOnePost: 'Post'
+    deleteManyPost: 'AffectedRowsOutput'
+    upsertOnePost: 'Post'
+    createOneComment: 'Comment'
+    updateOneComment: 'Comment'
+    updateManyComment: 'AffectedRowsOutput'
+    deleteOneComment: 'Comment'
+    deleteManyComment: 'AffectedRowsOutput'
+    upsertOneComment: 'Comment'
+    createOneSkill: 'Skill'
+    updateOneSkill: 'Skill'
+    updateManySkill: 'AffectedRowsOutput'
+    deleteOneSkill: 'Skill'
+    deleteManySkill: 'AffectedRowsOutput'
+    upsertOneSkill: 'Skill'
   },
   User: {
     id: 'String'
@@ -50,12 +115,44 @@ interface NexusPrismaOutputs {
     profileUrl: 'String'
     profileImageUrl: 'String'
     provider: 'AuthProvider'
+    posts: 'Post'
+    comments: 'Comment'
+    skills: 'Skill'
+  }
+  Post: {
+    id: 'Int'
+    author: 'User'
+    authorId: 'String'
+    title: 'String'
+    content: 'String'
+    thumbnailImageUrl: 'String'
+    comments: 'Comment'
+    createdAt: 'DateTime'
+    updatedAt: 'DateTime'
+  }
+  Comment: {
+    id: 'Int'
+    author: 'User'
+    authorId: 'String'
+    post: 'Post'
+    postId: 'Int'
+    content: 'String'
+    createdAt: 'DateTime'
+    updatedAt: 'DateTime'
+  }
+  Skill: {
+    id: 'Int'
+    name: 'String'
+    users: 'User'
   }
 }
 
 // Helper to gather all methods relative to a model
 interface NexusPrismaMethods {
   User: Typegen.NexusPrismaFields<'User'>
+  Post: Typegen.NexusPrismaFields<'Post'>
+  Comment: Typegen.NexusPrismaFields<'Comment'>
+  Skill: Typegen.NexusPrismaFields<'Skill'>
   Query: Typegen.NexusPrismaFields<'Query'>
   Mutation: Typegen.NexusPrismaFields<'Mutation'>
 }
