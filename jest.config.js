fe34f0a99637d95b2a3321b01a74655b455c2a35
leedavidcs <lee.david.cs@gmail.com>
2021-06-28 02:52:44 -0700

    Setup jest. Add storyshots. Cleanup dependencies

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	moduleNameMapper: {
+		"^@/(.*)$": "<rootDir>/src/$1",
+		"\\.svg$": "<rootDir>/src/test/mocks/svgr.mock.ts",
+		"\\.(css|less|scss|sss|styl)$": "identity-obj-proxy"
+	},
+	setupFiles: [
+		"dotenv/config",
+		"<rootDir>/src/test/mocks/fetch.mock.ts",
+		"<rootDir>/src/test//mocks/portal.mock.ts"
+	],
+	setupFilesAfterEnv: [
+		"./src/test/jest-setup.ts"
+	],
+	testEnvironment: "jsdom",
+	testMatch: [
+		"<rootDir>/**/*test.{ts,tsx}",
+		"<rootDir>/**/*test/index.{ts,tsx}"
+	],
+	testPathIgnorePatterns: [
+		"/.storybook/",
+		"/build",
+		"/dist/",
+		"/node_modules/"
+	],
+	transform: {
+		"^.+\\.[tj]sx?$": "babel-jest",
+		"^.+\\.mdx$": "@storybook/addon-docs/jest-transform-mdx"
+	}
+};
---
+++ src/test/stories.test.ts
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import initStoryshots from "@storybook/addon-storyshots";
+
+initStoryshots();
