5ffa663b12e9ae7e4fd46a5583518a53d747c75b
leedavidcs <lee.david.cs@gmail.com>
2021-07-05 16:36:49 -0700

    Make login-button more customizable

--- src/client/organisms/LoginButton/LoginButton.tsx
+++ src/client/organisms/LoginButton/LoginButton.tsx
@@ -1,25 +1,34 @@
 import { Button } from "@/client/atoms";
 import { useLogin } from "@/client/hooks";
 import { GitHubIcon } from "@/client/svgs";
-import React, { CSSProperties, FC } from "react";
+import React, { ComponentType, CSSProperties, FC, SVGAttributes } from "react";
 import styled from "styled-components";
 
-const StyledGitHubIcon = styled(GitHubIcon)`
-	margin-right: 8px;
+const Label = styled.span`
+	&:not(:first-child) {
+		margin-left: 8px;
+	}
 `;
 
 export interface LoginButtonProps {
 	className?: string;
+	icon?: Maybe<ComponentType<SVGAttributes<SVGSVGElement>>>;
+	label?: string;
 	style?: CSSProperties;
 }
 
-export const LoginButton: FC<LoginButtonProps> = ({ className, style }) => {
+export const LoginButton: FC<LoginButtonProps> = ({
+	className,
+	icon: Icon = GitHubIcon,
+	label = "Login",
+	style
+}) => {
 	const login = useLogin();
 
 	return (
 		<Button className={className} onClick={login} style={style} type="button">
-			<StyledGitHubIcon height={20} width={20} />
-			<span>Login</span>
+			{Icon && <Icon height={20} width={20} />}
+			<Label>{label}</Label>
 		</Button>
 	);
 };
