0160d716d17c8e55ccd6b3b91e64de25aae62e64
leedavidcs <lee.david.cs@gmail.com>
2021-07-08 04:46:49 -0700

    Add throttling and 1 retry to octokit

--- src/server/services/octokit/octokit-client.ts
+++ src/server/services/octokit/octokit-client.ts
@@ -1,8 +1,45 @@
+import { redis } from "@/server/redis";
+import { Logger } from "@/server/utils";
 import { createTokenAuth } from "@octokit/auth-token";
-import { graphql } from "@octokit/graphql";
+import { Octokit } from "@octokit/core";
+import { throttling } from "@octokit/plugin-throttling";
+import type { RequestOptions } from "@octokit/types";
+import Bottleneck from "bottleneck";
 import { oneLine } from "common-tags";
 
+const connection = new Bottleneck.IORedisConnection({ client: redis.instance });
+
+connection.on("error", (err) => {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+	Logger.error(err.toString?.());
+});
+
 export class OctokitClient {
+	public instance = new (Octokit.plugin(throttling))({
+		throttle: {
+			onRateLimit: (retryAfter: number, options: RequestOptions, octokit: Octokit) => {
+				octokit.log.warn(
+					`Request quota exhausted for request ${options.request} ${options.url}`
+				);
+
+				if (options.request?.retryCount === 0) {
+					octokit.log.info(`Retrying request after ${retryAfter} seconds`);
+
+					return true;
+				}
+
+				return false;
+			},
+			onAbuseLimit: (__: number, options: RequestOptions, octokit: Octokit) => {
+				octokit.log.warn(
+					`Abuse limit exceeded for request ${options.request} ${options.url}`
+				);
+			},
+			id: "makepurple",
+			Bottleneck
+		}
+	});
+
 	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 	public graphql(accessToken: string = process.env.GITHUB_ACCESS_TOKEN!) {
 		return <TVariables extends Record<string, unknown>>(
@@ -10,16 +47,20 @@ export class OctokitClient {
 			...exprs: any[]
 		) => {
 			const query = oneLine(strings, ...exprs);
-			const token = createTokenAuth(accessToken);
+			const auth = createTokenAuth(accessToken)();
 
-			return (variables?: TVariables) => {
-				return graphql({
+			return async (variables?: TVariables) => {
+				const { token } = await auth;
+
+				const response = await this.instance.graphql({
 					query,
 					...variables,
 					headers: {
 						Authorization: `token ${token}`
 					}
 				});
+
+				return response;
 			};
 		};
 	}
