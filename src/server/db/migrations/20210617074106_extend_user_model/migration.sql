f817a24be2d8ed8617eceecfec9d97bb17e2ee67
leedavidcs <lee.david.cs@gmail.com>
2021-07-09 04:10:40 -0700

    Update database schema for next-auth

--- src/server/db/migrations/20210617074106_extend_user_model/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateEnum
-CREATE TYPE "AuthProvider" AS ENUM ('GitHub');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "profileUrl" TEXT NOT NULL DEFAULT E'',
-ADD COLUMN     "provider" "AuthProvider" NOT NULL DEFAULT E'GitHub',
-ADD COLUMN     "username" TEXT NOT NULL DEFAULT E'';
