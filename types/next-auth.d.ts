8cd56c97a516fe1037e172b9a71282660b075609
leedavidcs <lee.david.cs@gmail.com>
2021-07-10 22:48:42 -0700

    Update user type

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -7,8 +7,8 @@ declare module "next-auth" {
 	interface Session {
 		user: {
 			/** The user's postal address. */
-			id?: number | null | undefined,
-			name?: string | null | undefined;
+			id?: number,
+			name?: string;
 			email?: string | null | undefined;
 			image?: string | null | undefined;
 		}
