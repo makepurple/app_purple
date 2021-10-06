1c10633c372f487576054924ccfd14667a4a6700
leedavidcs <lee.david.cs@gmail.com>
2021-10-06 00:39:59 -0700

    Add url validator utility

--- src/utils/url.util.ts
+++ src/utils/url.util.ts
@@ -11,4 +11,14 @@ export class UrlUtils {
 	public static appendQuery(url: string, params: Record<string, number | string>): string {
 		return `${url}${url.indexOf("?") === -1 ? "?" : "&"}${UrlUtils.toQuery(params)}`;
 	}
+
+	public static isValid(url: string): boolean {
+		try {
+			const _url = new URL(url);
+
+			return _url.protocol === "http:" || _url.protocol === "https:";
+		} catch {
+			return false;
+		}
+	}
 }
