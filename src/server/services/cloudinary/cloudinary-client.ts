fed749280d0d5273acdb69ed73dff46c0e392d53
leedavidcs <lee.david.cs@gmail.com>
2021-10-20 20:37:02 -0700

    Auto-generate cloudinary public ids

--- src/server/services/cloudinary/cloudinary-client.ts
+++ src/server/services/cloudinary/cloudinary-client.ts
@@ -1,5 +1,6 @@
 import type { FileUpload } from "@apollographql/graphql-upload-8-fork";
-import cloudinary from "cloudinary";
+import cloudinary, { UploadApiResponse } from "cloudinary";
+import { nanoid } from "nanoid";
 
 export class CloudinaryClient {
 	constructor() {
@@ -10,43 +11,49 @@ export class CloudinaryClient {
 		});
 	}
 
-	public uploadImageFile(file: FileUpload): Promise<string> {
-		const { filename } = file;
+	public static getImageFileName(filename: string): string {
+		return `${process.env.CLOUDINARY_CLOUD_NAME}/images/${filename}`;
+	}
 
-		return new Promise<string>((resolve, reject) => {
+	public uploadImageFile(file: FileUpload): Promise<UploadApiResponse> {
+		const filename: string = nanoid();
+
+		return new Promise<UploadApiResponse>((resolve, reject) => {
 			file.createReadStream().pipe(
 				cloudinary.v2.uploader.upload_stream(
 					{
 						type: "image",
-						public_id: `${process.env.CLOUDINARY_CLOUD_NAME}/images/${filename}`,
+						public_id: CloudinaryClient.getImageFileName(filename),
 						overwrite: true,
 						secure: true
 					},
 					(error, result) => {
 						if (error) return reject(error);
 						if (!result?.secure_url) return reject(new Error("No secure url"));
-						resolve(result.secure_url);
+						resolve(result);
 					}
 				)
 			);
 		});
 	}
 
+	public async retrieveImageFile(publicId: string) {
+		return await cloudinary.v2.api.resource(publicId);
+	}
+
 	public async deleteImageFile(filename: string): Promise<boolean> {
 		return await cloudinary.v2.uploader
-			.destroy(`${process.env.CLOUDINARY_CLOUD_NAME}/images/${filename}`)
+			.destroy(CloudinaryClient.getImageFileName(filename))
 			.then(() => true)
 			.catch(() => false);
 	}
 
-	public uploadImageDataUrl(filename: string, dataUrl: string): Promise<string> {
-		return cloudinary.v2.uploader
-			.upload(dataUrl, {
-				type: "image",
-				public_id: `${process.env.CLOUDINARY_CLOUD_NAME}/images/${filename}`,
-				overwrite: true,
-				secure: true
-			})
-			.then((result) => result.secure_url);
+	public uploadImageDataUrl(filename: string, dataUrl: string): Promise<UploadApiResponse> {
+		return cloudinary.v2.uploader.upload(dataUrl, {
+			type: "image",
+			public_id: CloudinaryClient.getImageFileName(filename),
+			overwrite: true,
+			secure: true
+		});
 	}
 }
