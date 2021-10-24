ab5fa3a7c8cb447835d1c39bae9c41a9da75203f
leedavidcs <lee.david.cs@gmail.com>
2021-10-24 15:13:01 -0700

    Change user name param

--- src/pages/[username]/index.tsx
+++ src/pages/[username]/index.tsx
@@ -1,17 +1,30 @@
 import { MainContainer } from "@/client/atoms";
+import { useGetPostsQuery } from "@/client/graphql";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import React from "react";
 import { styled } from "twin.macro";
+
+const BATCH_SIZE = 20;
 
 const Root = styled(MainContainer)``;
 
 export const Page: NextPage = () => {
 	const router = useRouter();
 
-	const { name } = router.query;
+	const { username } = router.query;
 
-	return <Root>{name}</Root>;
+	const [{ data }] = useGetPostsQuery({
+		requestPolicy: "cache-first",
+		variables: {
+			take: BATCH_SIZE,
+			where: {}
+		}
+	});
+
+	const posts = data?.posts ?? [];
+
+	return <Root>{username}</Root>;
 };
 
 export default Page;
