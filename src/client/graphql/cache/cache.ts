45fc823b25cb8f8b29cd3f5146db45af21647109
leedavidcs <lee.david.cs@gmail.com>
2021-10-24 21:26:42 -0700

    Add relay-pagination to posts query

--- src/client/graphql/cache/cache.ts
+++ src/client/graphql/cache/cache.ts
@@ -1,5 +1,12 @@
 import { cacheExchange } from "@urql/exchange-graphcache";
+import { relayPagination } from "@urql/exchange-graphcache/extras";
 
 export const createCache = () => {
-	return cacheExchange();
+	return cacheExchange({
+		resolvers: {
+			Query: {
+				posts: relayPagination()
+			}
+		}
+	});
 };
