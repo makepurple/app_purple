d59037c9b592da3f4ff751291be5704ea9597c09
leedavidcs <lee.david.cs@gmail.com>
2021-10-25 23:50:17 -0700

    Update links. Add mobile responsiveness

--- src/pages/[username]/index.tsx
+++ src/pages/[username]/index.tsx
@@ -1,10 +1,11 @@
-import { MainContainer } from "@/client/atoms";
+import { MainContainer, Paper } from "@/client/atoms";
 import { useGetPostsQuery } from "@/client/graphql";
 import { useRelayCursor } from "@/client/hooks";
-import { NonIdealState } from "@/client/molecules";
+import { NonIdealState, Tabs } from "@/client/molecules";
 import { LoadingPostCard, PostCard, UserInfoSideBar } from "@/client/organisms";
-import { NoteIcon } from "@/client/svgs";
+import { HexagonIcon, NoteIcon, RepoIcon } from "@/client/svgs";
 import { NextPage } from "next";
+import NextLink from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 import tw, { styled } from "twin.macro";
@@ -13,9 +14,29 @@ const BATCH_SIZE = 20;
 
 const Root = tw(MainContainer)`
 	flex
-	flex-row
+	flex-col-reverse
+	lg:flex-row
 	items-start
 	my-12
+`;
+
+const Content = tw.div`
+	flex
+	flex-col
+`;
+
+const LinksContainer = tw(Paper)`
+	relative
+	h-14
+`;
+
+const Links = tw(Tabs)`
+	absolute
+	left-6
+	bottom-0
+	right-6
+	xl:w-1/2
+	xl:right-auto
 `;
 
 const Posts = styled.div`
@@ -24,18 +45,24 @@ const Posts = styled.div`
 		flex
 		flex-col
 		items-stretch
+		mt-4
+		xl:mt-6
 	`}
 	& > *:not(:first-child) {
 		${tw`
-			mt-6
+			mt-4
+			xl:mt-6
 		`}
 	}
 `;
 
 const SideBar = tw(UserInfoSideBar)`
 	flex-shrink-0
-	w-96
-	ml-8
+	w-full
+	mb-6
+	lg:w-96
+	lg:ml-6
+	xl:ml-8
 `;
 
 export const Page: NextPage = () => {
@@ -62,22 +89,39 @@ export const Page: NextPage = () => {
 
 	return (
 		<Root>
-			<Posts>
-				{fetching ? (
-					Array.from({ length: 3 }, (_, i) => <LoadingPostCard key={i} />)
-				) : !posts.length ? (
-					<NonIdealState
-						title="There's nothing here"
-						subTitle="We couldn't find any posts"
-					>
-						<NoteIcon height={96} width={96} />
-					</NonIdealState>
-				) : (
-					posts.map((post, i) => (
-						<PostCard key={post.id} ref={getLoadMoreRef(i)} post={post} />
-					))
-				)}
-			</Posts>
+			<Content>
+				<LinksContainer>
+					<Links>
+						<NextLink href={`/${username}`} passHref>
+							<Tabs.Tab icon={NoteIcon} selected>
+								Posts
+							</Tabs.Tab>
+						</NextLink>
+						<NextLink href={`/${username}/repositories`} passHref>
+							<Tabs.Tab icon={RepoIcon}>Repositories</Tabs.Tab>
+						</NextLink>
+						<NextLink href={`/${username}/experience`} passHref>
+							<Tabs.Tab icon={HexagonIcon}>Experience</Tabs.Tab>
+						</NextLink>
+					</Links>
+				</LinksContainer>
+				<Posts>
+					{fetching ? (
+						Array.from({ length: 3 }, (_, i) => <LoadingPostCard key={i} />)
+					) : !posts.length ? (
+						<NonIdealState
+							title="There's nothing here"
+							subTitle="We couldn't find any posts"
+						>
+							<NoteIcon height={96} width={96} />
+						</NonIdealState>
+					) : (
+						posts.map((post, i) => (
+							<PostCard key={post.id} ref={getLoadMoreRef(i)} post={post} />
+						))
+					)}
+				</Posts>
+			</Content>
 			<SideBar username={username as string} />
 		</Root>
 	);
