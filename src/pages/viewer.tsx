46b1beea4e875f6036d3e17a26c440e6f91949a6
leedavidcs <lee.david.cs@gmail.com>
2021-11-01 01:22:56 -0700

    Add experimental viewer page

---
+++ src/pages/viewer.tsx
@@ -0,0 +1,44 @@
+import {
+	addUrqlState,
+	createUrqlClient,
+	GetMyUserDocument,
+	GetMyUserQuery,
+	GetMyUserQueryVariables,
+	useGetMyUserQuery
+} from "@/client/graphql";
+import { NextUtils } from "@/utils";
+import type { NextPage } from "next";
+import { getSession } from "next-auth/client";
+import React from "react";
+import { ssrExchange } from "urql";
+
+export const getServerSideProps = NextUtils.castSSRProps(async (ctx) => {
+	const ssr = ssrExchange({ isClient: false });
+
+	const { req } = ctx;
+
+	const session = await getSession(ctx);
+
+	const urqlClient = createUrqlClient({ req, ssr });
+
+	await urqlClient
+		.query<GetMyUserQuery, GetMyUserQueryVariables>(GetMyUserDocument)
+		.toPromise()
+		.then((result) => result.data?.viewer ?? null);
+
+	return addUrqlState(ssr, {
+		props: { session }
+	});
+});
+
+export const Page: NextPage = () => {
+	const [{ data }] = useGetMyUserQuery({
+		requestPolicy: "cache-only"
+	});
+
+	const viewer = data?.viewer;
+
+	return <div>{JSON.stringify(viewer ?? {}, null, 4)}</div>;
+};
+
+export default Page;
