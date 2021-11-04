f22cd1ab789b4c3dc633100e03853f968ba28b06
leedavidcs <lee.david.cs@gmail.com>
2021-11-04 00:34:38 -0700

    Make FormButton styled composable

--- src/client/atoms/FormButton/FormButton.tsx
+++ src/client/atoms/FormButton/FormButton.tsx
@@ -1,13 +1,16 @@
 import { Button, ButtonProps } from "@/client/atoms/Button";
 import { FormContext } from "@/client/atoms/Form/context";
 import React, { FC, useContext } from "react";
+import { styled } from "twin.macro";
 
 export type FormButtonProps = ButtonProps;
 
-export const FormButton: FC<FormButtonProps> = (props) => {
+export const FormButton = styled(((props: FormButtonProps) => {
 	const form = useContext(FormContext);
 
 	const disabled = props.disabled || form.disabled;
 
 	return <Button {...props} disabled={disabled} />;
-};
+}) as FC<FormButtonProps>)``;
+
+FormButton.displayName = "FormButton";
