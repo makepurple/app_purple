19ef83e3784e3ddeb3434b92719dcadd0a7de363
leedavidcs <lee.david.cs@gmail.com>
2021-11-06 04:36:25 -0700

    Make FormHelperText easier to use

--- src/client/atoms/FormHelperText/FormHelperText.tsx
+++ src/client/atoms/FormHelperText/FormHelperText.tsx
@@ -1,6 +1,6 @@
 import { FormGroupContext } from "@/client/atoms/FormGroup/context";
 import { InferComponentProps } from "@/client/types";
-import React, { FC, useContext } from "react";
+import React, { FC, forwardRef, ReactNode, useContext } from "react";
 import tw, { styled } from "twin.macro";
 
 const Root = styled.div<{ error?: boolean }>`
@@ -13,12 +13,23 @@ const Root = styled.div<{ error?: boolean }>`
 	${({ error }) => (error ? tw`text-red-600` : tw`text-gray-500`)}
 `;
 
-export type FormHelperTextProps = InferComponentProps<typeof Root>;
+export type FormHelperTextProps = InferComponentProps<"div"> & {
+	error?: ReactNode;
+};
 
-export const FormHelperText = styled(((props: FormHelperTextProps) => {
-	const context = useContext(FormGroupContext);
+export const FormHelperText = styled(
+	// eslint-disable-next-line react/display-name
+	forwardRef<HTMLDivElement, FormHelperTextProps>((props, ref) => {
+		const context = useContext(FormGroupContext);
 
-	const error = props.error ?? context.error;
+		const error = !!props.error ?? context.error;
 
-	return <Root {...props} error={error} />;
-}) as FC<FormHelperTextProps>)<FormHelperTextProps>``;
+		if (!props.error || !props.children) return null;
+
+		return (
+			<Root {...props} ref={ref} error={error}>
+				{props.error ?? props.children}
+			</Root>
+		);
+	})
+)<FormHelperTextProps>``;
