6265a00bf56eb054e05990644a207e305d9ca94b
leedavidcs <lee.david.cs@gmail.com>
2021-11-14 16:35:53 -0800

    Improve exports for ComboBox

--- src/client/atoms/ComboBox/ComboBox.tsx
+++ src/client/atoms/ComboBox/ComboBox.tsx
@@ -1,14 +1,28 @@
 import { UseComboBoxState } from "@/client/hooks";
 import { InferComponentProps } from "@/client/types";
+import { ObjectUtils } from "@/utils";
 import React, { ComponentType, FC } from "react";
+import { ComboBoxInput } from "./ComboBoxInput";
+import { ComboBoxLoadingState } from "./ComboBoxLoadingState";
+import { ComboBoxOption } from "./ComboBoxOption";
+import { ComboBoxSelect } from "./ComboBoxSelect";
 
 export type ComboBoxProps = InferComponentProps<"div"> &
 	UseComboBoxState<any> & {
 		as?: string | ComponentType<any>;
 	};
 
-export const ComboBox: FC<ComboBoxProps> = (props) => {
+const _ComboBox: FC<ComboBoxProps> = (props) => {
 	const { as: Type = "div", combobox, ...divProps } = props;
 
 	return <Type {...divProps} {...combobox.getComboboxProps({ ...divProps })} />;
 };
+
+_ComboBox.displayName = "ComboBox";
+
+export const ComboBox = ObjectUtils.setStatic(_ComboBox, {
+	Input: ComboBoxInput,
+	LoadingState: ComboBoxLoadingState,
+	Option: ComboBoxOption,
+	Select: ComboBoxSelect
+});
