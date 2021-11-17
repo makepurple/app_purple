09e9258153da3f5bb132c156036f96021c81dcbf
leedavidcs <lee.david.cs@gmail.com>
2021-11-17 00:32:06 -0800

    Upgrade framer-motion major. Patch storybook errors

--- .storybook/main.js
+++ .storybook/main.js
@@ -28,11 +28,24 @@ module.exports = {
 		 * Change this to use what is in next.config.js for webpack5, when this
 		 * config eventually uses webpack5
 		 * @author David Lee
-		 * @date June 28
+		 * @date June 28, 2021
 		 */
 		config.node = {
 			fs: "empty",
 		};
+
+		/**
+		 * !HACK
+		 * @description framer-motion ^5.0.0 is breaking storybook without this
+		 * @see (@link: https://github.com/framer/motion/issues/1307#issuecomment-966827629)
+		 * @author David Lee
+		 * @date November 16, 2021
+		 */
+		config.module.rules.push({
+			type: "javascript/auto",
+			test: /\.mjs$/,
+			include: /node_modules/,
+		});
 		
 		return addWebpackAlias({ "@": path.resolve(__dirname, "../src") })(config);
 	}
