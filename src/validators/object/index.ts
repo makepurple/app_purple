066899b2ac9cec70053099c233775904d5f6a601
leedavidcs <lee.david.cs@gmail.com>
2021-12-09 01:35:21 -0800

    Add validator for ExperienceCreateInput

--- src/validators/object/index.ts
+++ src/validators/object/index.ts
@@ -1,4 +1,5 @@
 export * from "./DocumentEditorValue";
+export * from "./ExperienceCreateInput";
 export * from "./PostDraftUpdateInput";
 export * from "./PostPublishInput";
 export * from "./PostUpdateInput";
