1b3fc41cc5d15979603e8b4f0b512dcab78efa16
leedavidcs <lee.david.cs@gmail.com>
2021-12-11 16:13:00 -0800

    Add Checkbox and FormLabel

---
+++ src/client/atoms/Checkbox/Checkbox.tsx
@@ -0,0 +1,20 @@
+import { InferComponentProps } from "@/client/types";
+import tw, { styled } from "twin.macro";
+
+export type CheckboxProps = InferComponentProps<typeof Checkbox>;
+
+export const Checkbox = styled.input`
+	${tw`
+		form-checkbox
+		h-6
+		w-6
+		rounded
+		border-gray-400
+		text-indigo-500
+		focus:ring-indigo-500
+	`}
+`;
+
+Checkbox.defaultProps = {
+	type: "checkbox"
+};
