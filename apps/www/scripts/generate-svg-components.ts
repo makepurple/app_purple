fbff54f017cf292669a501813c1541342023633b
leedavidcs <lee.david.cs@gmail.com>
2021-12-17 00:19:26 -0800

    Move generate svgs script

--- apps/www/scripts/generate-svg-components.ts
+++ apps/www/scripts/generate-svg-components.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { transform } from "@svgr/core";
 import { stripIndents } from "common-tags";
 import fs from "fs-extra";
@@ -9,7 +10,7 @@ const dirname: string = process.env.PROJECT_DIRNAME
 	: __dirname;
 
 const sourcePath: string = path.resolve(dirname, "../public/static/svgs");
-const componentPath: string = path.resolve(dirname, "../src/client/svgs");
+const componentPath: string = path.resolve(dirname, "../src/svgs");
 
 const generateComponents = () => {
 	const filenames = glob.sync(`${sourcePath}/**.svg`);
@@ -37,10 +38,7 @@ const generateComponents = () => {
 				// 1. Clean SVG files using SVGO
 				// 2. Generate JSX
 				// 3. Format the result using Prettier
-				plugins: [
-					"@svgr/plugin-jsx",
-					"@svgr/plugin-prettier"
-				],
+				plugins: ["@svgr/plugin-jsx", "@svgr/plugin-prettier"],
 				svgoConfig: {
 					plugins: [{ removeViewBox: false }]
 				},
@@ -56,20 +54,17 @@ const generateComponents = () => {
 			fs.appendFileSync(
 				`${componentPath}/index.tsx`,
 				`${stripIndents`
+					import type { InferComponentProps } from "@makepurple/typings"
 					import * as React from "react";
 					import type { Ref, SVGProps } from "react";
-					import type { InferComponentProps } from "@/client/types"
 
 					export type SvgIconComponent = typeof ${componentName};
 					export type SvgIconComponentProps = InferComponentProps<SvgIconComponent>;
 				`}\n\n`
-			)
+			);
 		}
 
-		fs.appendFileSync(
-			`${componentPath}/index.tsx`,
-			`${componentCode}\n`
-		);
+		fs.appendFileSync(`${componentPath}/index.tsx`, `${componentCode}\n`);
 	});
 };
 
