d29afb97bde9a6f822d4db6e1b51a880a9fbc026
leedavidcs <lee.david.cs@gmail.com>
2021-12-22 00:37:32 -0800

    Improve ExperienceCreateInput schema

--- packages/validators/src/object/ExperienceCreateInput.ts
+++ packages/validators/src/object/ExperienceCreateInput.ts
@@ -3,18 +3,29 @@ import Schema, { array, DateType, number, string } from "computed-types";
 
 export const ExperienceCreateInput = Schema({
 	endDate: Schema.either(
-		DateType,
+		DateType.optional(),
 		Schema({
 			month: number.gte(0, "Invalid month").lte(12, "Invalid year"),
 			year: number.gte(0, "Invalid year")
-		})
+		}).optional()
 	).optional(),
-	highlights: array
-		.of(string.max(512, "Cannot be longer than 512 characters"))
-		.max(12, "Cannot have more than 16 highlights"),
-	location: string.max(255, "Cannot be longer than 255 characters"),
-	organizationName: string.max(255, "Cannot be longer than 255 charaters"),
-	positionName: string.max(255, "Cannot be longer than 255 characters"),
+	highlights: Schema.either(
+		array
+			.error("Required")
+			.of(string.max(512, "Cannot be longer than 512 characters"))
+			.max(8, "Cannot have more than 8 highlights"),
+		array
+			.error("Required")
+			.of(
+				Schema({
+					value: string.max(512, "Cannot be longer than 512 characters")
+				})
+			)
+			.max(8, "Cannot have more than 8 highlights")
+	),
+	location: string.max(255, "Cannot be longer than 255 characters").optional(),
+	organizationName: string.error("Required").max(255, "Cannot be longer than 255 charaters"),
+	positionName: string.error("Required").max(255, "Cannot be longer than 255 characters"),
 	startDate: Schema.either(
 		DateType,
 		Schema({
