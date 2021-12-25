906b27d1233257fa41fe339b335df46ac0edb0a8
leedavidcs <lee.david.cs@gmail.com>
2021-12-24 23:50:03 -0800

    Remove unused charts

--- packages/charts/src/XYChart/Tooltip/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./context";
-export * from "./Tooltip";
-export * from "./TooltipCrosshair";
-export * from "./TooltipPanel";
-export * from "./TooltipPoint";
--- packages/charts/types.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference types="@makepurple/typings" />
