d0ab620fe2b234589897759b90d1d9ec5cda5a0c
leedavidcs <lee.david.cs@gmail.com>
2022-01-02 00:10:56 -0800

    Fix deleting root paragraph node on DocumentEditor

--- packages/components/src/molecules/DocumentEditor/plugins/withKeyCommands.ts
+++ packages/components/src/molecules/DocumentEditor/plugins/withKeyCommands.ts
@@ -4,23 +4,24 @@ export const withKeyCommands = (editor: Editor) => {
 	const { deleteBackward, insertBreak, isVoid } = editor;
 
 	editor.deleteBackward = (...args) => {
-		if (!editor.selection) {
-			return deleteBackward(...args);
-		}
+		if (!editor.selection) return deleteBackward(...args);
 
 		const [parentNode, parentPath] = Editor.parent(editor, editor.selection.focus.path);
 
-		if (isVoid(parentNode as Element) || !Node.string(parentNode).length) {
+		if (
+			editor.children.length > 1 &&
+			(isVoid(parentNode as Element) || !Node.string(parentNode).length)
+		) {
 			Transforms.removeNodes(editor, { at: parentPath });
-		} else {
-			deleteBackward(...args);
+
+			return;
 		}
+
+		deleteBackward(...args);
 	};
 
 	editor.insertBreak = (...args) => {
-		if (!editor.selection) {
-			return insertBreak(...args);
-		}
+		if (!editor.selection) return insertBreak(...args);
 
 		const [parentNode, parentPath] = Editor.parent(editor, editor.selection.focus.path);
 		const nextPath = Path.next(parentPath);
