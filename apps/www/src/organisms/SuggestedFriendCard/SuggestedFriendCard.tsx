092725cfdf3cb1d56300198ad379a5dde78e95a9
leedavidcs <lee.david.cs@gmail.com>
2022-01-05 06:35:30 -0800

    Add tags data to SuggestedFriendCard

--- apps/www/src/organisms/SuggestedFriendCard/SuggestedFriendCard.tsx
+++ apps/www/src/organisms/SuggestedFriendCard/SuggestedFriendCard.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Divider, GitHubAvatarImage, Paper } from "@makepurple/components";
+import { Avatar, Divider, GitHubAvatarImage, Paper, Tags } from "@makepurple/components";
 import { RenderComponentProps as MasonicRenderProps } from "masonic";
 import NextImage from "next/image";
 import NextLink from "next/link";
@@ -6,18 +6,22 @@ import React, { CSSProperties, forwardRef } from "react";
 import tw from "twin.macro";
 import { SuggestedFriendCardUserFragment } from "../../graphql";
 
+const MAX_SKILLS_SHOWN = 5;
+
 const Root = tw(Paper)`
 	flex
 	flex-col
 	items-stretch
-	overflow-hidden
 `;
 
 const ThumbnailContainer = tw.div`
 	relative
+	rounded-t-lg
+	overflow-hidden
 `;
 
-const PostThumbnail = tw.div`
+const PostThumbnail = tw.a`
+	block
 	aspect-h-9
 	aspect-w-16
 `;
@@ -72,6 +76,13 @@ const UserDescription = tw.div`
 	text-gray-500
 `;
 
+const UserSkills = tw.div`
+	flex
+	flex-col
+	gap-1.5
+	[& > *]:text-sm
+`;
+
 export interface SuggestedFriendCardProps
 	extends MasonicRenderProps<SuggestedFriendCardUserFragment> {
 	className?: string;
@@ -84,28 +95,42 @@ export const SuggestedFriendCard = forwardRef<HTMLDivElement, SuggestedFriendCar
 
 		const post = user.posts.nodes[0];
 
+		const skills = user.skills.nodes;
+		const desiredSkills = user.desiredSkills.nodes;
+
+		const skillsExtra = skills.length - MAX_SKILLS_SHOWN;
+		const desiredSkillsExtra = desiredSkills.length - MAX_SKILLS_SHOWN;
+
 		return (
 			<Root ref={ref} className={className} style={style}>
 				<ThumbnailContainer>
 					{post?.thumbnailUrl && (
-						<PostThumbnail>
-							<NextImage
-								alt={post.title ?? ""}
-								src={post.thumbnailUrl}
-								layout="fill"
-								objectFit="cover"
-							/>
-						</PostThumbnail>
+						<NextLink
+							href="/[userName]/[postTitle]"
+							as={`/${user.name}/${post.urlSlug}`}
+							passHref
+						>
+							<PostThumbnail tabIndex={-1} tw="mb-0.5">
+								<NextImage
+									alt={post.title ?? ""}
+									src={post.thumbnailUrl}
+									layout="fill"
+									objectFit="cover"
+								/>
+							</PostThumbnail>
+						</NextLink>
 					)}
 					{user.image && (
-						<UserAvatar border={3}>
-							<GitHubAvatarImage
-								alt={user.name}
-								src={user.image}
-								height={56}
-								width={56}
-							/>
-						</UserAvatar>
+						<NextLink href="/[userName]" as={`/${user.name}`} passHref>
+							<UserAvatar tabIndex={-1} border={3}>
+								<GitHubAvatarImage
+									alt={user.name}
+									src={user.image}
+									height={56}
+									width={56}
+								/>
+							</UserAvatar>
+						</NextLink>
 					)}
 				</ThumbnailContainer>
 				<Content>
@@ -133,6 +158,40 @@ export const SuggestedFriendCard = forwardRef<HTMLDivElement, SuggestedFriendCar
 							<UserName>{user.name}</UserName>
 							{user.description && (
 								<UserDescription tw="mt-1">{user.description}</UserDescription>
+							)}
+							{(!!skills.length || !!desiredSkills.length) && (
+								<UserSkills tw="mt-3">
+									{!!skills.length && (
+										<Tags type="positive">
+											{skills.slice(0, MAX_SKILLS_SHOWN).map((skill) => (
+												<Tags.Tag key={skill.id} id={skill.id} tw="px-1">
+													{skill.name}
+												</Tags.Tag>
+											))}
+											{skillsExtra > 0 && (
+												<Tags.Tag id="see-more" tw="px-1">
+													+{skillsExtra} other
+													{skillsExtra === 1 ? "" : "s"}
+												</Tags.Tag>
+											)}
+										</Tags>
+									)}
+									{!!desiredSkills.length && (
+										<Tags type="negative">
+											{desiredSkills.slice(0, 5).map((skill) => (
+												<Tags.Tag key={skill.id} id={skill.id} tw="px-1">
+													{skill.name}
+												</Tags.Tag>
+											))}
+											{desiredSkillsExtra > 0 && (
+												<Tags.Tag id="see-more" tw="px-1">
+													+{desiredSkillsExtra} other
+													{desiredSkillsExtra === 1 ? "" : "s"}
+												</Tags.Tag>
+											)}
+										</Tags>
+									)}
+								</UserSkills>
 							)}
 						</UserInfo>
 					</NextLink>
