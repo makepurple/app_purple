8935f0fe5434b6d4a1ff14ad9871bc386d78b1b1
leedavidcs <lee.david.cs@gmail.com>
2022-01-17 00:21:49 -0800

    Update user data seeding

--- packages/prisma/prisma/seed/data/users.ts
+++ packages/prisma/prisma/seed/data/users.ts
@@ -5,40 +5,40 @@ import { skills } from "./skills";
 
 faker.seed(1);
 
-const names: readonly string[] = [
-	"dsklyar",
-	"jameshly",
-	"haejinjo",
-	"igorsorokin66",
-	"onionpancakes",
-	"gaearon",
-	"Owlsarecool1",
-	"benawad",
-	"sampoder",
-	"SadisticCompiler",
-	"jaredpalmer",
-	"fiizzy",
-	"leerob",
-	"luxonauta",
-	"jasonbarone",
-	"marvelefe",
-	"lucasdinocloud",
-	"bluemix",
-	"stevejcox",
-	"PawelNackiewicz",
-	"rizkimcitra"
+const names: readonly [name: string, image: string][] = [
+	["dsklyar", "https://avatars.githubusercontent.com/u/12689384"],
+	["jameshly", "https://avatars.githubusercontent.com/u/19769647"],
+	["haejinjo", "https://avatars.githubusercontent.com/u/23015394"],
+	["igorsorokin66", "https://avatars.githubusercontent.com/u/5367066"],
+	["onionpancakes", "https://avatars.githubusercontent.com/u/639985"],
+	["gaearon", "https://avatars.githubusercontent.com/u/810438"],
+	["Owlsarecool1", "https://avatars.githubusercontent.com/u/39209930"],
+	["benawad", "https://avatars.githubusercontent.com/u/7872329"],
+	["sampoder", "https://avatars.githubusercontent.com/u/39828164"],
+	["SadisticCompiler", "https://avatars.githubusercontent.com/u/11155306"],
+	["jaredpalmer", "https://avatars.githubusercontent.com/u/4060187"],
+	["fiizzy", "https://avatars.githubusercontent.com/u/34406562"],
+	["leerob", "https://avatars.githubusercontent.com/u/9113740"],
+	["luxonauta", "https://avatars.githubusercontent.com/u/48334856"],
+	["jasonbarone", "https://avatars.githubusercontent.com/u/545055"],
+	["marvelefe", "https://avatars.githubusercontent.com/u/56586020"],
+	["lucasdinocloud", "https://avatars.githubusercontent.com/u/79989292"],
+	["bluemix", "https://avatars.githubusercontent.com/u/3332274"],
+	["stevejcox", "https://avatars.githubusercontent.com/u/15239550"],
+	["PawelNackiewicz", "https://avatars.githubusercontent.com/u/31191198"],
+	["rizkimcitra", "https://avatars.githubusercontent.com/u/62492410"]
 ];
 
 const skillIds = skills.map((skill) => skill.id!);
 
-export const users: readonly Prisma.UserCreateInput[] = names.map((name, i) => {
+export const users: readonly Prisma.UserCreateInput[] = names.map(([name, image], i) => {
 	const userSkillIds: readonly string[] = [
 		...new Set(
 			faker.random.arrayElements(
 				skillIds,
 				Math.floor(
 					faker.datatype.number({
-						min: 0,
+						min: 2,
 						max: skillIds.length - 1
 					})
 				)
@@ -47,7 +47,7 @@ export const users: readonly Prisma.UserCreateInput[] = names.map((name, i) => {
 	];
 
 	const remainingSkillIds = skillIds.filter(
-		(skillId) => !!userSkillIds.find((id) => id === skillId)
+		(skillId) => !userSkillIds.some((id) => id === skillId)
 	);
 
 	const userDesiredSkillIds: readonly string[] = [
@@ -67,12 +67,33 @@ export const users: readonly Prisma.UserCreateInput[] = names.map((name, i) => {
 	return {
 		id: `User_${i}`,
 		email: `${name}@testmakepurple.com`,
+		image,
 		name,
 		skills: {
-			create: userSkillIds.map((skillId) => ({ skillId }))
+			connectOrCreate: userSkillIds.map((skillId) => ({
+				where: {
+					skillId_userId: {
+						skillId,
+						userId: `User_${i}`
+					}
+				},
+				create: {
+					skillId
+				}
+			}))
 		},
 		desiredSkills: {
-			create: userDesiredSkillIds.map((skillId) => ({ skillId }))
+			connectOrCreate: userDesiredSkillIds.map((skillId) => ({
+				where: {
+					skillId_userId: {
+						skillId,
+						userId: `User_${i}`
+					}
+				},
+				create: {
+					skillId
+				}
+			}))
 		}
 	};
 });
