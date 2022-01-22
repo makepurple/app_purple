6dabdef1d0298d9cefce55c3b18a53fb93d4c242
leedavidcs <lee.david.cs@gmail.com>
2022-01-22 00:10:42 -0800

    Fix error emitted for missing playground plugins

--- packages/server/src/graphql/plugins/playground.plugin.ts
+++ packages/server/src/graphql/plugins/playground.plugin.ts
@@ -1,8 +1,8 @@
-import { ApolloServerPlugin } from "./types";
 import {
 	ApolloServerPluginLandingPageDisabled,
 	ApolloServerPluginLandingPageGraphQLPlayground
-} from ".pnpm/apollo-server-core@3.5.0_graphql@15.7.2/node_modules/apollo-server-core";
+} from "apollo-server-core";
+import { ApolloServerPlugin } from "./types";
 
 export const playgroundPlugin = (): ApolloServerPlugin =>
 	process.env.NODE_ENV === "development"
