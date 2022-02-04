d6d6a3623cf4f47b49ccd9cb34ddc821020392a2
leedavidcs <lee.david.cs@gmail.com>
2022-02-03 22:48:36 -0800

    Improve loading repository and skill cards

--- apps/www/src/organisms/LoadingSkillCard/LoadingSkillCard.tsx
+++ apps/www/src/organisms/LoadingSkillCard/LoadingSkillCard.tsx
@@ -4,7 +4,7 @@ import tw from "twin.macro";
 
 const Root = tw.div`
 	flex
-	flex-col
+	flex-row
 	items-start
 	py-4
 `;
@@ -17,20 +17,18 @@ export interface LoadingSkillCardProps {
 export const LoadingSkillCard: FC<LoadingSkillCardProps> = ({ className, style }) => {
 	return (
 		<Root className={className} style={style}>
-			<Skeleton tw="h-4.5 w-32" />
-			<Skeleton tw="h-4 w-80 mt-2" />
-			<div tw="flex flex-wrap gap-1.5 mt-3">
-				<Skeleton tw="h-6 w-24" />
-				<Skeleton tw="h-6 w-24" />
-				<Skeleton tw="h-6 w-24" />
-			</div>
-			<div tw="flex flex-wrap gap-3.5 mt-3">
-				<Skeleton tw="h-3.5 w-16" />
-				<Skeleton tw="h-3.5 w-16" />
-				<Skeleton tw="h-3.5 w-16" />
-				<Skeleton tw="h-3.5 w-16" />
-				<Skeleton tw="h-3.5 w-16" />
-				<Skeleton tw="h-3.5 w-16" />
+			<Skeleton tw="height[72px] width[72px] rounded-md mr-6" />
+			<div tw="flex flex-col items-start">
+				<Skeleton tw="h-4.5 w-32" />
+				<Skeleton tw="h-4 w-80 mt-2" />
+				<div tw="flex flex-wrap gap-3.5 mt-4">
+					<Skeleton tw="h-3.5 w-16" />
+					<Skeleton tw="h-3.5 w-16" />
+					<Skeleton tw="h-3.5 w-16" />
+					<Skeleton tw="h-3.5 w-16" />
+					<Skeleton tw="h-3.5 w-16" />
+					<Skeleton tw="h-3.5 w-16" />
+				</div>
 			</div>
 		</Root>
 	);
