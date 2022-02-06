b960bbabcb3078912c614648c8fb99d6af35f118
leedavidcs <lee.david.cs@gmail.com>
2022-02-06 11:46:11 -0800

    Access OrderedListItem from OrderedList

--- packages/components/src/atoms/OrderedList/OrderedList.tsx
+++ packages/components/src/atoms/OrderedList/OrderedList.tsx
@@ -1,6 +1,12 @@
+import { ObjectUtils } from "@makepurple/utils";
 import tw from "twin.macro";
+import { OrderedListItem } from "./OrderedListItem";
 
-export const OrderedList = tw.ol`
+const _OrderedList = tw.ol`
 	list-decimal
 	list-inside
 `;
+
+export const OrderedList = ObjectUtils.setStatic(_OrderedList, {
+	Item: OrderedListItem
+});
