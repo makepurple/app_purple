c1b837b1790a78cc3bbbd3b52ff90019d0d85f8b
leedavidcs <lee.david.cs@gmail.com>
2022-02-11 00:21:08 -0800

    Add initial skill page layout containers

--- apps/www/src/pages/s/[skillOwner]/[skillName].tsx
+++ apps/www/src/pages/s/[skillOwner]/[skillName].tsx
@@ -1,8 +1,51 @@
+import { MainContainer } from "@makepurple/components";
 import { NextPage } from "next";
 import React from "react";
+import tw from "twin.macro";
+
+const Root = tw(MainContainer)`
+	flex
+	flex-col
+	items-stretch
+	my-12
+	lg:flex-row-reverse
+	lg:items-start
+`;
+
+const SideBar = tw.div`
+	flex-shrink-0
+	flex
+	flex-col
+	items-stretch
+	gap-4
+	w-full
+	mb-6
+	lg:w-96
+	lg:ml-4
+	xl:gap-6
+	xl:ml-6
+`;
+
+const Posts = tw.div`
+	flex-grow
+	flex
+	flex-col
+	items-stretch
+	gap-4
+	xl:gap-6
+`;
 
 export const Page: NextPage = () => {
-	return <div>Skill page works~!</div>;
+	return (
+		<Root>
+			<SideBar>
+				<div />
+			</SideBar>
+			<Posts>
+				<div />
+			</Posts>
+		</Root>
+	);
 };
 
 export default Page;
