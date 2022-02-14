a3f6bf18c0d5b48c792bd30fb7c01c40020c4ab6
leedavidcs <lee.david.cs@gmail.com>
2022-02-13 22:29:22 -0800

    Rewrite promise.util to use async/await. Add some

--- packages/utils/src/promise.util.ts
+++ packages/utils/src/promise.util.ts
@@ -3,34 +3,28 @@ export interface PromiseUtilsMapOptions {
 }
 
 export class PromiseUtils {
-	public static map = <I, R>(
+	public static map = async <I, R>(
 		items: I[],
 		options: Maybe<Partial<PromiseUtilsMapOptions>>,
 		mapFn: (item: I, index: number) => Promise<R>
 	): Promise<R[]> => {
 		const { concurrency }: PromiseUtilsMapOptions = { concurrency: 1, ...(options ?? {}) };
 
-		if (items.length === 0) return Promise.resolve([]);
+		if (items.length === 0) return [];
 
 		const results: R[] = Array(items.length).fill(null);
 		const trueConcurrencyLimit: number = Math.min(items.length, concurrency);
 
 		let index: number;
 
-		const iterablePromiseFn = (item: I, i: number): Promise<I | undefined> => {
-			return new Promise<I | undefined>((resolve, reject) => {
-				return mapFn(item, i)
-					.then((result) => {
-						results[i] = result;
+		const iterablePromiseFn = async (item: I, i: number): Promise<I | undefined> => {
+			const result = await mapFn(item, i);
 
-						const isOutOfItems: boolean = index++ >= items.length - 1;
+			results[i] = result;
 
-						return isOutOfItems
-							? resolve(undefined)
-							: resolve(iterablePromiseFn(items[index], index));
-					})
-					.catch(reject);
-			});
+			const isOutOfItems: boolean = index++ >= items.length - 1;
+
+			return isOutOfItems ? undefined : iterablePromiseFn(items[index], index);
 		};
 
 		const rateLimitedProcessor = Array(trueConcurrencyLimit)
@@ -42,6 +36,45 @@ export class PromiseUtils {
 			});
 
 		return Promise.all(rateLimitedProcessor).then(() => results);
+	};
+
+	public static some = async <I>(
+		items: I[],
+		options: Maybe<Partial<PromiseUtilsMapOptions>>,
+		predicate: (item: I, index: number) => Promise<boolean>
+	): Promise<boolean> => {
+		const { concurrency }: PromiseUtilsMapOptions = { concurrency: 1, ...(options ?? {}) };
+
+		if (items.length === 0) return false;
+
+		const trueConcurrencyLimit: number = Math.min(items.length, concurrency);
+
+		let index: number;
+		let didFind: boolean = false;
+
+		const iterablePromiseFn = async (item: I, i: number): Promise<boolean> => {
+			const result = await predicate(item, i);
+
+			if (didFind || result) {
+				didFind = true;
+
+				return true;
+			}
+
+			const isOutOfItems: boolean = index++ >= items.length - 1;
+
+			return isOutOfItems ? false : iterablePromiseFn(items[index], index);
+		};
+
+		const rateLimitedProcessor = Array.from({ length: trueConcurrencyLimit }, (_, i) => {
+			index = i;
+
+			return iterablePromiseFn(items[i], i);
+		});
+
+		await Promise.all(rateLimitedProcessor);
+
+		return didFind;
 	};
 
 	public static wait(msTime: number): Promise<void> {
