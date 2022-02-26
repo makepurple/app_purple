a8287b3b11526c8349b4a140b87ee73bd2c61bc0
leedavidcs <lee.david.cs@gmail.com>
2022-02-26 03:00:28 -0800

    Restrict CodeExample skills to at most 5

--- packages/validators/src/object/CodeExampleCreateInput.ts
+++ packages/validators/src/object/CodeExampleCreateInput.ts
@@ -7,7 +7,11 @@ export const CodeExampleCreateInput = Schema({
 	description: string.trim().max(140, "Max 140 character limit").strictOptional(),
 	language: Schema.enum(CodeLanguage, "Unsupported language").error("Required"),
 	primarySkill: SkillWhereUniqueInput.strictOptional(),
-	skills: array.of(SkillWhereUniqueInput).min(1, "Required").error("Required"),
+	skills: array
+		.of(SkillWhereUniqueInput)
+		.min(1, "Required")
+		.max(5, "Maximum of 5 allowed")
+		.error("Required"),
 	title: string
 		.min(12, "Must be at least 12 characters")
 		.max(100, "100 character limit")
--- packages/validators/src/object/CodeExampleUpdateInput.ts
+++ packages/validators/src/object/CodeExampleUpdateInput.ts
@@ -7,7 +7,11 @@ export const CodeExampleUpdateInput = Schema({
 	description: string.trim().max(140, "Max 140 character limit").strictOptional(),
 	language: Schema.enum(CodeLanguage, "Unsupported language").strictOptional(),
 	primarySkill: SkillWhereUniqueInput.strictOptional(),
-	skills: array.of(SkillWhereUniqueInput).min(1, "Required").strictOptional(),
+	skills: array
+		.of(SkillWhereUniqueInput)
+		.min(1, "Required")
+		.max(5, "Maximum of 5 allowed")
+		.strictOptional(),
 	title: string
 		.min(12, "Must be at least 12 characters")
 		.max(100, "100 character limit")
