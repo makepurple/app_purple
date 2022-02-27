97633e4631f5f6f634317682126e6ecb4d1d4a63
leedavidcs <lee.david.cs@gmail.com>
2022-02-27 00:23:27 -0800

    Added story for codeExampleTitle page

---
+++ apps/stories/src/pages/[userName]/snippets/codeExampleTitle.stories.tsx
@@ -0,0 +1,64 @@
+import { SiteWideLayout } from "@makepurple/www";
+import {
+	GetCodeExample_mock,
+	GetNotificationCounts_mock,
+	GetPostDraft_mock,
+	GetSiteWideSideDrawer_mock,
+	GetUserFriendRequestCount_mock,
+	GetUserInfoSideBar_mock
+} from "@makepurple/www/src/graphql/mocks";
+import { PageProps } from "@makepurple/www/src/page-props/[userName]/snippets/[codeExampleTitle]";
+import Page from "@makepurple/www/src/pages/[userName]/snippets/[codeExampleTitle]";
+import type { Meta, Story } from "@storybook/react";
+import React from "react";
+import { getOperationName, Operation } from "urql";
+
+export default {
+	title: "pages/[userName]/snippets/[codeExampleTitle]",
+	component: Page,
+	decorators: [
+		(Story) => (
+			<SiteWideLayout>
+				<Story />
+			</SiteWideLayout>
+		)
+	]
+} as Meta;
+
+const Template: Story<PageProps> = (args) => {
+	return <Page {...args} />;
+};
+Template.args = {};
+Template.parameters = {
+	layout: "fullscreen",
+	nextRouter: {
+		query: {
+			userName: "leedavidcs",
+			codeExampleTitle: "useeffectonce"
+		}
+	}
+};
+
+export const Standard = Template.bind({});
+Standard.args = { ...Template.args };
+Standard.parameters = {
+	...Template.parameters,
+	urql: (op: Operation) => {
+		switch (getOperationName(op.query)) {
+			case "GetCodeExample":
+				return { data: GetCodeExample_mock };
+			case "GetNotificationCounts":
+				return { data: GetNotificationCounts_mock };
+			case "GetPostDraft":
+				return { data: GetPostDraft_mock };
+			case "GetSiteWideSideDrawer":
+				return { data: GetSiteWideSideDrawer_mock };
+			case "GetUserFriendRequestCount":
+				return { data: GetUserFriendRequestCount_mock };
+			case "GetUserInfoSideBar":
+				return { data: GetUserInfoSideBar_mock };
+			default:
+				return {};
+		}
+	}
+};
