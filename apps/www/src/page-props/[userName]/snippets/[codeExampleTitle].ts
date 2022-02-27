ae2326fb54d7c5396207e69eb2635546570a18ee
leedavidcs <lee.david.cs@gmail.com>
2022-02-27 00:22:22 -0800

    Added ssr to codeExampleTitle page

---
+++ apps/www/src/page-props/[userName]/snippets/[codeExampleTitle].ts
@@ -0,0 +1,53 @@
+import { InferGetServerSidePropsType } from "next";
+import { getSession } from "next-auth/react";
+import { ssrExchange } from "urql";
+import {
+	addUrqlState,
+	createUrqlClient,
+	GetCodeExampleDocument,
+	GetCodeExampleQuery,
+	GetCodeExampleQueryVariables,
+	GetPostDraftDocument,
+	GetPostDraftQuery,
+	GetPostDraftQueryVariables,
+	GetUserInfoSideBarDocument,
+	GetUserInfoSideBarQuery,
+	GetUserInfoSideBarQueryVariables
+} from "../../../graphql";
+import { NextUtils } from "../../../utils";
+
+export const pageProps = NextUtils.castSSRProps(async (ctx) => {
+	const { query, req } = ctx;
+
+	const ssr = ssrExchange({ isClient: false });
+	const urqlClient = createUrqlClient({ req, ssr });
+
+	const authorName = query.authorName as string;
+	const urlSlug = query.codeExampleTitle as string;
+
+	await Promise.all([
+		urqlClient
+			.query<GetCodeExampleQuery, GetCodeExampleQueryVariables>(GetCodeExampleDocument, {
+				authorName,
+				urlSlug
+			})
+			.toPromise(),
+		urqlClient
+			.query<GetPostDraftQuery, GetPostDraftQueryVariables>(GetPostDraftDocument)
+			.toPromise(),
+		urqlClient
+			.query<GetUserInfoSideBarQuery, GetUserInfoSideBarQueryVariables>(
+				GetUserInfoSideBarDocument,
+				{ name: query.userName as string }
+			)
+			.toPromise()
+	]);
+
+	return addUrqlState(ssr, {
+		props: {
+			session: await getSession(ctx)
+		}
+	});
+});
+
+export type PageProps = InferGetServerSidePropsType<typeof pageProps>;
--- apps/www/src/pages/[userName]/snippets/[codeExampleTitle].tsx
+++ apps/www/src/pages/[userName]/snippets/[codeExampleTitle].tsx
@@ -1,15 +1,35 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import React from "react";
+import { useGetCodeExampleQuery } from "../../../graphql";
 import { UserPageLayout } from "../../../organisms";
+import { pageProps, PageProps } from "../../../page-props/[userName]/snippets/[codeExampleTitle]";
 
-export const Page: NextPage = () => {
+export const getServerSideProps = pageProps;
+
+export const Page: NextPage<PageProps> = () => {
 	const router = useRouter();
 
-	const userName = router?.query.userName as string;
+	const authorName = router?.query.userName as string;
+	const urlSlug = router?.query.codeExampleTitle as string;
+
+	const [{ data }] = useGetCodeExampleQuery({
+		requestPolicy: "cache-first",
+		variables: { authorName, urlSlug }
+	});
+
+	const codeExample = data?.codeExample;
+
+	/**
+	 * TODO
+	 * @description Return 404 error page eventually
+	 * @author David Lee
+	 * @date February 27, 2022
+	 */
+	if (!codeExample) return null;
 
 	return (
-		<UserPageLayout selectedTab="snippets" userName={userName}>
+		<UserPageLayout selectedTab="snippets" userName={authorName}>
 			<div>Snippet page works~!</div>
 		</UserPageLayout>
 	);
