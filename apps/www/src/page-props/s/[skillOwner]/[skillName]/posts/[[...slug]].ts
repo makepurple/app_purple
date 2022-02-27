b0077ac2fd5cad1b5daa9fecf518604cc3a08c34
leedavidcs <lee.david.cs@gmail.com>
2022-02-26 23:27:43 -0800

    Add sorting to skill posts page-props

--- apps/www/src/page-props/s/[skillOwner]/[skillName]/posts/[[...slug]].ts
+++ apps/www/src/page-props/s/[skillOwner]/[skillName]/posts/[[...slug]].ts
@@ -1,3 +1,4 @@
+import { dayjs } from "@makepurple/utils";
 import { InferGetServerSidePropsType } from "next";
 import { getSession } from "next-auth/react";
 import { ssrExchange } from "urql";
@@ -12,7 +13,10 @@ import {
 	GetPostsQueryVariables,
 	GetSkillInfoSideBarDocument,
 	GetSkillInfoSideBarQuery,
-	GetSkillInfoSideBarQueryVariables
+	GetSkillInfoSideBarQueryVariables,
+	PostOrderByInput,
+	PostWhereInput,
+	SortOrder
 } from "../../../../../graphql";
 import { NextUtils } from "../../../../../utils";
 
@@ -23,6 +27,27 @@ export const pageProps = NextUtils.castSSRProps(async (ctx) => {
 
 	const skillName = query.skillName as string;
 	const skillOwner = query.skillOwner as string;
+	const slug = query.slug as (string | undefined)[];
+
+	const [sort, criteria] = slug;
+
+	const [orderBy, where] = ((): [PostOrderByInput | undefined, PostWhereInput] => {
+		switch (sort) {
+			case "top": {
+				const span = criteria ?? "week";
+
+				const gte = ["week", "month", "year"].some((range) => range === span)
+					? dayjs().subtract(1, span).toDate()
+					: span === "all"
+					? undefined
+					: dayjs().subtract(1, "week").toDate();
+
+				return [{ upvoters: { _count: SortOrder.Desc } }, { publishedAt: { gte } }];
+			}
+			default:
+				return [{ publishedAt: SortOrder.Desc }, {}];
+		}
+	})();
 
 	const ssr = ssrExchange({ isClient: false });
 	const urqlClient = createUrqlClient({ req, ssr });
@@ -32,7 +57,9 @@ export const pageProps = NextUtils.castSSRProps(async (ctx) => {
 			.query<GetPostsQuery, GetPostsQueryVariables>(GetPostsDocument, {
 				after: null,
 				first: BATCH_SIZE,
+				orderBy,
 				where: {
+					...where,
 					skills: {
 						some: {
 							name: { equals: skillName },
