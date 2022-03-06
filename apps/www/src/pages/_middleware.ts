372ff68c657e705e0e1ca24ca2d55befc27e3d28
leedavidcs <lee.david.cs@gmail.com>
2022-03-05 21:52:48 -0800

    Add global middleware to prevent showing gdpr cookie footer for non-eu

---
+++ apps/www/src/pages/_middleware.ts
@@ -0,0 +1,28 @@
+import { isEuMember } from "is-eu-member";
+import ms from "ms";
+import { NextMiddleware, NextRequest, NextResponse } from "next/server";
+
+const GDPR_COOKIE_MAX_AGE_SECONDS = ms("1y") / 1_000;
+
+const middleware: NextMiddleware = (req: NextRequest) => {
+	const country = req.geo?.country;
+	const acceptGdprConsent: boolean = !!country && !isEuMember(country);
+
+	const response = NextResponse.next();
+
+	if (!acceptGdprConsent) return response;
+
+	/**
+	 * !HACK
+	 * @description Auto-accept gdpr for any countries not in the EU
+	 * @author David Lee
+	 * @date March 5, 2022
+	 */
+	response.cookie("gdpr-consent", "true", {
+		maxAge: GDPR_COOKIE_MAX_AGE_SECONDS
+	});
+
+	return response;
+};
+
+export default middleware;
