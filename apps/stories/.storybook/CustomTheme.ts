813ad32f10784b6d0be3dfd95db8185dc740d653
leedavidcs <lee.david.cs@gmail.com>
2022-03-06 18:22:14 -0800

    Added custom branding to storybook

---
+++ apps/stories/.storybook/CustomTheme.ts
@@ -0,0 +1,8 @@
+import { create } from "@storybook/theming";
+
+export default create({
+	base: "light",
+	brandTitle: "MakePurple Design",
+	brandUrl: "https://makepurple.com",
+	brandImage: "/static/pngs/BrandLogo.png"
+});
