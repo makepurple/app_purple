5982d434ba16bacbb9a3721c4bc1363355b1c86c
leedavidcs <lee.david.cs@gmail.com>
2022-03-07 23:18:44 -0800

    Follow and unfollow users on github when doing the same on the app

--- packages/server/src/graphql/nexus/mutations/followUser.ts
+++ packages/server/src/graphql/nexus/mutations/followUser.ts
@@ -1,5 +1,6 @@
 import { FollowType, UserActivityType } from "@prisma/client";
 import { arg, mutationField, nonNull } from "nexus";
+import { octokit } from "../../../services";
 import { PrismaUtils } from "../../../utils";
 
 export const followUser = mutationField("followUser", {
@@ -10,34 +11,73 @@ export const followUser = mutationField("followUser", {
 	authorize: (parent, args, { user }) => {
 		return !!user;
 	},
-	resolve: async (parent, args, { prisma, user }) => {
+	resolve: async (parent, args, { octokit: graphql, prisma, user }) => {
 		if (!user) throw new Error();
 
-		const record = await prisma.followUser
-			.create({
-				data: {
-					follow: {
-						create: {
-							activities: {
-								create: {
-									type: UserActivityType.FollowUser,
-									user: { connect: { id: user.id } }
-								}
-							},
-							type: FollowType.User,
-							user: { connect: { id: user.id } }
-						}
-					},
-					follower: { connect: { id: user.id } },
-					following: {
-						connect: PrismaUtils.nonNull(args.where)
+		const result = await prisma.followUser.create({
+			data: {
+				follow: {
+					create: {
+						activities: {
+							create: {
+								type: UserActivityType.FollowUser,
+								user: { connect: { id: user.id } }
+							}
+						},
+						type: FollowType.User,
+						user: { connect: { id: user.id } }
 					}
 				},
-				include: {
-					follow: true
+				follower: { connect: { id: user.id } },
+				following: {
+					connect: PrismaUtils.nonNull(args.where)
 				}
+			},
+			include: {
+				follow: true,
+				following: true
+			}
+		});
+
+		const record = result.follow;
+		const following = result.following;
+
+		const githubId = await graphql`
+			query GetGitHubUserToFollow($login: String!) {
+				user(login: $login) {
+					id
+				}
+			}
+		`
+			.cast<octokit.GetGitHubUserToFollowQuery, octokit.GetGitHubUserToFollowQueryVariables>({
+				login: following.name
 			})
-			.then((result) => result.follow);
+			.then((res) => res.user?.id ?? null)
+			.catch(() => null);
+
+		if (githubId) {
+			await graphql`
+				mutation FollowGitHubUser($githubId: ID!) {
+					followUser(input: { userId: $githubId }) {
+						user {
+							id
+							login
+						}
+					}
+				}
+			`
+				.cast<octokit.FollowGitHubUserMutation, octokit.FollowGitHubUserMutationVariables>({
+					githubId
+				})
+				/**
+				 * !HACK
+				 * @description Try to follow the user on GitHub, and don't mind the error if not
+				 * able to.
+				 * @author David Lee
+				 * @date March 7, 2022
+				 */
+				.catch(() => null);
+		}
 
 		return { record };
 	}
--- packages/server/src/graphql/nexus/mutations/unfollowUser.ts
+++ packages/server/src/graphql/nexus/mutations/unfollowUser.ts
@@ -1,4 +1,5 @@
 import { arg, mutationField, nonNull } from "nexus";
+import { octokit } from "../../../services";
 import { NotFoundError, PrismaUtils } from "../../../utils";
 
 export const unfollowUser = mutationField("unfollowUser", {
@@ -9,7 +10,7 @@ export const unfollowUser = mutationField("unfollowUser", {
 	authorize: (parent, args, { user }) => {
 		return !!user;
 	},
-	resolve: async (parent, args, { prisma, user }) => {
+	resolve: async (parent, args, { octokit: graphql, prisma, user }) => {
 		if (!user) throw new Error();
 
 		const following = await prisma.user.findUnique({
@@ -44,6 +45,47 @@ export const unfollowUser = mutationField("unfollowUser", {
 
 			return deleted;
 		});
+
+		const githubId = await graphql`
+			query GetGitHubUserToUnfollow($login: String!) {
+				user(login: $login) {
+					id
+				}
+			}
+		`
+			.cast<
+				octokit.GetGitHubUserToUnfollowQuery,
+				octokit.GetGitHubUserToUnfollowQueryVariables
+			>({
+				login: following.name
+			})
+			.then((res) => res.user?.id ?? null)
+			.catch(() => null);
+
+		if (githubId) {
+			await graphql`
+				mutation UnfollowGitHubUser($githubId: ID!) {
+					unfollowUser(input: { userId: $githubId }) {
+						user {
+							id
+							login
+						}
+					}
+				}
+			`
+				.cast<
+					octokit.UnfollowGitHubUserMutation,
+					octokit.UnfollowGitHubUserMutationVariables
+				>()
+				/**
+				 * !HACK
+				 * @description Try to unfollow the user on GitHub, and don't mind the error if not
+				 * able to.
+				 * @author David Lee
+				 * @date March 7, 2022
+				 */
+				.catch(() => null);
+		}
 
 		return { record };
 	}
