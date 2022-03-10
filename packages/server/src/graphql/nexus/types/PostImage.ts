f07e2452bd7e40d6c6e13449612747f222410b5e
leedavidcs <lee.david.cs@gmail.com>
2022-03-09 22:58:22 -0800

    Port PostImage off nexus-prisma

--- packages/server/src/graphql/nexus/types/PostImage.ts
+++ packages/server/src/graphql/nexus/types/PostImage.ts
@@ -1,13 +1,21 @@
-import { NexusPrisma } from "@makepurple/prisma/nexus";
 import { objectType } from "nexus";
 
 export const PostImage = objectType({
-	name: NexusPrisma.PostImage.$name,
-	description: NexusPrisma.PostImage.$description,
+	name: "PostImage",
 	definition: (t) => {
-		t.field(NexusPrisma.PostImage.id);
-		t.field(NexusPrisma.PostImage.post);
-		t.field(NexusPrisma.PostImage.postId);
-		t.field(NexusPrisma.PostImage.url);
+		t.nonNull.id("id");
+		t.nonNull.field("post", {
+			type: "Post",
+			resolve: (parent, args, { prisma }) => {
+				return prisma.post.findUnique({
+					where: {
+						id: parent.postId
+					},
+					rejectOnNotFound: true
+				});
+			}
+		});
+		t.nonNull.string("postId");
+		t.nonNull.string("url");
 	}
 });
