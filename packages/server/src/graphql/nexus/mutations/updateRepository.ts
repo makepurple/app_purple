94c59bb1669dab6fa9acbababc3760b4d5ac1e93
leedavidcs <lee.david.cs@gmail.com>
2022-03-13 05:41:36 -0700

    Have updateRepository verify skills. Optimize updateRepository

--- packages/server/src/graphql/nexus/mutations/updateRepository.ts
+++ packages/server/src/graphql/nexus/mutations/updateRepository.ts
@@ -1,6 +1,7 @@
 import { PromiseUtils } from "@makepurple/utils";
 import { arg, mutationField, nonNull } from "nexus";
-import { PrismaUtils } from "../../../utils";
+import { octokit } from "../../../services";
+import { Logger, PrismaUtils } from "../../../utils";
 
 export const updateRepository = mutationField("updateRepository", {
 	type: nonNull("UpdateRepositoryPayload"),
@@ -19,7 +20,7 @@ export const updateRepository = mutationField("updateRepository", {
 
 		return true;
 	},
-	resolve: async (parent, args, { prisma, user }) => {
+	resolve: async (parent, args, { octokit: graphql, prisma, user }) => {
 		if (!user) throw new Error();
 
 		const repository = await prisma.repository.findUnique({
@@ -28,17 +29,50 @@ export const updateRepository = mutationField("updateRepository", {
 
 		if (!repository) throw new Error("Repository could not be found");
 
-		const skills = args.data.skills ?? [];
+		const skillIds = (args.data.skills ?? [])
+			.filter((skill) => !!skill.id)
+			.map((skill) => skill.id) as string[];
 
-		const skillIds = skills.filter((skill) => !!skill.id).map((skill) => skill.id) as string[];
-
-		const skillNameOwners = skills
+		const skillNameOwners = (args.data.skills ?? [])
 			.filter((skill) => !!skill.name_owner)
 			.map((skill) => skill.name_owner) as { name: string; owner: string }[];
 
-		const skillsById = await prisma.skill.findMany({
-			where: { id: { in: skillIds } }
+		const existingSkills = await prisma.skill.findMany({
+			where: { OR: [{ id: { in: skillIds } }, ...skillNameOwners] }
 		});
+
+		const toCreateSkills = skillNameOwners.filter(
+			({ name, owner }) =>
+				!existingSkills.some((skill) => skill.name === name && skill.owner === owner)
+		);
+
+		const verifySkill = async (name: string, owner: string): Promise<boolean> => {
+			const verified = await graphql`
+				query VerifySkill($name: String!, $owner: String!) {
+					repository(followRenames: false, name: $name, owner: $owner) {
+						id
+					}
+				}
+			`
+				.cast<octokit.VerifySkillQuery, octokit.VerifySkillQueryVariables>({
+					name,
+					owner
+				})
+				.then((result) => !!result.repository)
+				.catch(() => false);
+
+			return verified;
+		};
+
+		if (toCreateSkills.length) Logger.info("Verifying skills:", toCreateSkills);
+
+		const verified = await PromiseUtils.every(
+			toCreateSkills,
+			{ concurrency: 2 },
+			async (skill) => await verifySkill(skill.name, skill.owner)
+		);
+
+		if (!verified) throw new Error("All skills must be from GitHub");
 
 		const record = await prisma.$transaction(async (transaction) => {
 			await transaction.repository.update({
@@ -46,19 +80,21 @@ export const updateRepository = mutationField("updateRepository", {
 				where: PrismaUtils.nonNull(args.where)
 			});
 
-			const skillsByNameOwner = await PromiseUtils.map(
-				skillNameOwners,
-				{ concurrency: 2 },
-				async ({ name, owner }) => {
-					return await transaction.skill.upsert({
-						where: { name_owner: { name, owner } },
-						create: { name, owner },
-						update: {}
-					});
-				}
-			);
+			await transaction.organization.createMany({
+				data: toCreateSkills.map(({ owner }) => ({ name: owner })),
+				skipDuplicates: true
+			});
 
-			const skillsToConnect = [...skillsById, ...skillsByNameOwner];
+			await transaction.skill.createMany({
+				data: toCreateSkills.map(({ name, owner }) => ({ name, owner })),
+				skipDuplicates: true
+			});
+
+			const newSkills = await transaction.skill.findMany({
+				where: { OR: toCreateSkills }
+			});
+
+			const skillsToConnect = [...existingSkills, ...newSkills];
 
 			return await transaction.repository.update({
 				data: {
@@ -71,8 +107,7 @@ export const updateRepository = mutationField("updateRepository", {
 								}
 							},
 							create: {
-								skillId: skill.id,
-								repositoryId: repository.id
+								skillId: skill.id
 							}
 						}))
 					}
