8616eacd10cf2e4ee45b81fded88cfb7200b13f4
leedavidcs <lee.david.cs@gmail.com>
2022-03-12 18:53:05 -0800

    Change logger condition to be more strict for silent

--- packages/server/src/utils/logger.util.ts
+++ packages/server/src/utils/logger.util.ts
@@ -1,6 +1,6 @@
 import Winston, { createLogger, transports } from "winston";
 
 export const Logger: Winston.Logger = createLogger({
-	silent: process.env.NODE_ENV === "production",
+	silent: process.env.NODE_ENV !== "development",
 	transports: [new transports.Console()]
 });
