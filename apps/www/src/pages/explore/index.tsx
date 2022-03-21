47ab7c324fe5042695c5a4e684dc2f050aa5b3a2
leedavidcs <lee.david.cs@gmail.com>
2022-03-20 17:48:04 -0700

    Memoize masonic props to fix infinite render loop

--- apps/www/src/pages/explore/index.tsx
+++ apps/www/src/pages/explore/index.tsx
@@ -1,11 +1,11 @@
 import { MainContainer, NonIdealState } from "@makepurple/components";
 import { useRelayCursor } from "@makepurple/hooks";
-import { Masonry } from "masonic";
+import { Masonry, RenderComponentProps } from "masonic";
 import { NextPage } from "next";
 import { useSession } from "next-auth/react";
-import React, { useMemo, useRef } from "react";
+import React, { useCallback, useMemo, useRef } from "react";
 import tw from "twin.macro";
-import { SuggestFriendsDocument } from "../../graphql";
+import { SuggestedFriendCardUserFragment, SuggestFriendsDocument } from "../../graphql";
 import { SuggestedFriendCard } from "../../organisms";
 import { PageProps, pageProps } from "../../page-props/explore";
 import { PersonIcon } from "../../svgs";
@@ -55,6 +55,15 @@ export const Page: NextPage<PageProps> = ({ jitterSeed }) => {
 		[data?.suggestFriends.nodes]
 	);
 
+	const itemKey = useCallback((item: SuggestedFriendCardUserFragment) => item.id, []);
+
+	const renderCard = useCallback(
+		(friendProps: RenderComponentProps<SuggestedFriendCardUserFragment>) => {
+			return <SuggestedFriendCard ref={getRef(friendProps.index)} {...friendProps} />;
+		},
+		[getRef]
+	);
+
 	return (
 		<Root>
 			{!suggestedFriends.length ? (
@@ -68,13 +77,11 @@ export const Page: NextPage<PageProps> = ({ jitterSeed }) => {
 				<Masonry
 					columnGutter={8}
 					columnWidth={240}
-					itemKey={(item) => item.id}
+					itemKey={itemKey}
 					items={suggestedFriends}
 					overscanBy={5}
 					tabIndex={-1}
-					render={(friendProps) => (
-						<SuggestedFriendCard ref={getRef(friendProps.index)} {...friendProps} />
-					)}
+					render={renderCard}
 				/>
 			)}
 		</Root>
