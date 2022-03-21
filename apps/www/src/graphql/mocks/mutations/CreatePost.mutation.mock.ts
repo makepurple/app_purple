84cbccab2e0fd1d6f225b29375cb5a6cebbf67af
leedavidcs <lee.david.cs@gmail.com>
2022-03-20 21:57:04 -0700

    Fix build errors

--- apps/www/src/graphql/mocks/mutations/CreatePost.mutation.mock.ts
+++ apps/www/src/graphql/mocks/mutations/CreatePost.mutation.mock.ts
@@ -5,7 +5,6 @@ export const CreatePost_mock: CreatePostMutation = {
 	__typename: "Mutation",
 	createPost: {
 		__typename: "CreatePostPayload",
-		cursor: Post_fragment_mock.id,
 		record: Post_fragment_mock as any
 	}
 };
