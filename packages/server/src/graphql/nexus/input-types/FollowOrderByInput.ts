5166fa12f085207ae2393e2c7054d2b38601e8e0
leedavidcs <lee.david.cs@gmail.com>
2022-03-24 03:46:12 -0700

    Added ordering by follow createdAt

--- packages/server/src/graphql/nexus/input-types/FollowOrderByInput.ts
+++ packages/server/src/graphql/nexus/input-types/FollowOrderByInput.ts
@@ -3,6 +3,7 @@ import { inputObjectType } from "nexus";
 export const FollowOrderByInput = inputObjectType({
 	name: "FollowOrderByInput",
 	definition: (t) => {
+		t.field("createdAt", { type: "SortOrder" });
 		t.field("type", { type: "SortOrder" });
 	}
 });
