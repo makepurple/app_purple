6c0abd442ed8f8ec01796c681d3bd45ad83cc0d2
leedavidcs <lee.david.cs@gmail.com>
2022-03-26 03:35:44 -0700

    Simplify requestFriendship

--- packages/server/src/graphql/nexus/mutations/requestFriendship.ts
+++ packages/server/src/graphql/nexus/mutations/requestFriendship.ts
@@ -1,6 +1,6 @@
 import { dayjs } from "@makepurple/utils";
 import { arg, mutationField, nonNull } from "nexus";
-import { NotFoundError, PrismaUtils } from "../../../utils";
+import { PrismaUtils } from "../../../utils";
 
 export const requestFriendship = mutationField("requestFriendship", {
 	type: nonNull("RequestFriendshipPayload"),
@@ -14,10 +14,10 @@ export const requestFriendship = mutationField("requestFriendship", {
 		if (!user) throw new Error();
 
 		const toFriend = await prisma.user.findUnique({
-			where: PrismaUtils.nonNull(args.where)
+			where: PrismaUtils.nonNull(args.where),
+			rejectOnNotFound: true
 		});
 
-		if (!toFriend) throw new NotFoundError("User could not be found");
 		if (toFriend.name === user.name) throw new Error("Cannot friend yourself!");
 
 		const existing = await prisma.friendship.findUnique({
@@ -32,7 +32,14 @@ export const requestFriendship = mutationField("requestFriendship", {
 			}
 		});
 
-		if (existing?.rejectedAt && existing.rejectedAt > dayjs().subtract(6, "months").toDate()) {
+		if (existing?.rejectedAt && dayjs().diff(dayjs(existing.rejectedAt), "months") <= 6) {
+			/**
+			 * @description Prevent friend requesting the same user, but return the requested
+			 * user, so that the requester assumes the request goes through (disincentivizes
+			 * requesting again)
+			 * @author David Lee
+			 * @date March 26, 2022
+			 */
 			return { record: existing.friending };
 		}
 
