f4926cff34b69e0b4fec6066eeea775542e1291f
leedavidcs <lee.david.cs@gmail.com>
2022-03-27 03:22:33 -0700

    Use more interesting ChatRoom story

--- apps/stories/src/organisms/ChatRoom.stories.tsx
+++ apps/stories/src/organisms/ChatRoom.stories.tsx
@@ -1,11 +1,19 @@
+import { faker } from "@faker-js/faker";
+import { useWillMount } from "@makepurple/hooks";
 import { PromiseUtils } from "@makepurple/utils";
 import { ChatRoom, ChatRoomProps } from "@makepurple/www";
-import { GetChat_mock, SuggestViewerFriends_mock } from "@makepurple/www/src/graphql/mocks";
+import {
+	ChatMessage_fragment_mock,
+	GetChat_mock,
+	SuggestViewerFriends_mock
+} from "@makepurple/www/src/graphql/mocks";
 import { action } from "@storybook/addon-actions";
 import type { Meta, Story } from "@storybook/react";
 import ms from "ms";
 import React from "react";
 import { getOperationName, Operation } from "urql";
+
+const MESSAGES_SIZE = 5;
 
 export default {
 	title: "organisms/ChatRoom",
@@ -13,6 +21,10 @@ export default {
 } as Meta;
 
 const Template: Story<ChatRoomProps> = (args) => {
+	useWillMount(() => {
+		faker.seed(1);
+	});
+
 	return <ChatRoom {...args} tw="height[600px]" />;
 };
 Template.args = {};
@@ -23,10 +35,66 @@ Template.parameters = {
 		operationName && action(operationName)(op.variables);
 
 		switch (operationName) {
-			case "GetChat":
+			case "GetChat": {
+				const messages = Array.from({ length: MESSAGES_SIZE }, () => {
+					const senderId = faker.datatype.boolean() ? "0" : "1";
+
+					return {
+						...ChatMessage_fragment_mock,
+						content: faker.datatype.boolean()
+							? ChatMessage_fragment_mock.content
+							: [
+									{
+										type: "paragraph",
+										children: [
+											{
+												text: faker.lorem.sentences(
+													faker.datatype.number({ min: 1, max: 5 })
+												)
+											}
+										]
+									}
+							  ],
+						id: faker.random.alphaNumeric(30),
+						sender: {
+							...ChatMessage_fragment_mock.sender,
+							name: faker.datatype.boolean()
+								? ChatMessage_fragment_mock.sender.name
+								: faker.internet.userName(),
+							id: senderId
+						},
+						senderId
+					};
+				});
+
 				await PromiseUtils.wait(ms("2s"));
 
-				return { data: GetChat_mock };
+				return {
+					data: {
+						...GetChat_mock,
+						chat: {
+							...GetChat_mock.chat,
+							messages: {
+								__typename: "ChatMessageConnection",
+								pageInfo: {
+									__typename: "PageInfo",
+									endCursor: null,
+									hasNextPage: false,
+									hasPreviousPage: false,
+									startCursor: null
+								},
+								totalCount: MESSAGES_SIZE,
+								edges: messages.map((message) => ({
+									__typename: "ChatMessageEdge",
+									cursor: message.id,
+									node: message
+								})),
+								nodes: messages
+							}
+						}
+					}
+				};
+			}
 			case "SuggestViewerFriends":
 				return { data: SuggestViewerFriends_mock };
 			default:
