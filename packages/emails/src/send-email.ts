c3e0ce1bdea8d1a58c4bb2e783cf65b8cba6dc2f
leedavidcs <lee.david.cs@gmail.com>
2022-03-27 22:47:09 -0700

    Change sendEmail to accept non-postmark props

--- packages/emails/src/send-email.ts
+++ packages/emails/src/send-email.ts
@@ -1,19 +1,24 @@
-import type { Message, MessageSendingResponse } from "postmark/dist/client/models";
 import { postmark } from "./postmark";
 
-export type SendEmailMessage = OptionalProps<Message, "From">;
+export interface SendEmailParams {
+	to: string;
+	subject: string;
+	body: string;
+}
 
-export const sendEmail = async (
-	email: SendEmailMessage
-): Promise<Maybe<MessageSendingResponse>> => {
-	if (process.env.NODE_ENV !== "production") return null;
+export const sendEmail = async (email: SendEmailParams): Promise<boolean> => {
+	if (process.env.NODE_ENV !== "production") return true;
 
-	if (!process.env.POSTMARK_FROM_EMAIL) return null;
+	if (!process.env.POSTMARK_FROM_EMAIL) {
+		throw new Error('Missing "from" field for sending emails!');
+	}
 
 	await postmark.sendEmail({
 		From: process.env.POSTMARK_FROM_EMAIL,
-		...email
+		Subject: email.subject,
+		To: email.to,
+		HtmlBody: email.body
 	});
 
-	return undefined;
+	return false;
 };
