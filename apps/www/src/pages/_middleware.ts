4d268b1482c089a8348af4414f7eb974385dec33
leedavidcs <lee.david.cs@gmail.com>
2022-04-02 00:00:24 -0700

    Added rate-limiting

--- apps/www/src/pages/_middleware.ts
+++ apps/www/src/pages/_middleware.ts
@@ -1,16 +1,39 @@
+import { redis } from "@makepurple/server";
+import RateLimiter from "async-ratelimiter";
 import { isEuMember } from "is-eu-member";
 import ms from "ms";
-import { NextMiddleware, NextRequest, NextResponse } from "next/server";
+import type { NextMiddleware, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { NextUtils } from "../utils";
 
 const GDPR_COOKIE_MAX_AGE_SECONDS = ms("1y") / 1_000;
 
-const middleware: NextMiddleware = (req: NextRequest) => {
+const rateLimiter = new RateLimiter({
+	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+	db: redis.instance!,
+	duration: ms("1m"),
+	max: 60
+});
+
+const middleware: NextMiddleware = async (req: NextRequest) => {
 	const { pathname } = req.nextUrl;
 
-	if (pathname.includes("/api")) return NextResponse.next();
+	if (pathname.startsWith("/api")) return NextResponse.next();
+	if (pathname.startsWith("/banned")) return NextResponse.next();
+	if (pathname.startsWith("/rate-limited")) return NextResponse.next();
+
+	if (!req.ip) {
+		return NextResponse.redirect(NextUtils.getUrl(req, "/500"));
+	}
+
+	const limit = await rateLimiter.get({ id: req.ip });
+
+	if (!limit.remaining) {
+		return NextResponse.redirect(NextUtils.getUrl(req, "/rate-limited"));
+	}
 
 	const country = req.geo?.country;
-	const acceptGdprConsent: boolean = !!country && !isEuMember(country);
+	const acceptGdprConsent = !!country && !isEuMember(country);
 
 	if (!acceptGdprConsent) return NextResponse.next();
 
