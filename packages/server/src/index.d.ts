2e75789d05d46321290301f98e7113094854c6ac
leedavidcs <lee.david.cs@gmail.com>
2022-04-01 23:53:55 -0700

    Expose redis from server package

--- packages/server/src/index.d.ts
+++ packages/server/src/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="./graphql/nexus/generated/typegen.gen" />
 
 export * from "./graphql";
+export * from "./redis";
 export * from "./utils";
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./graphql";
+export * from "./redis";
 export * from "./utils";
