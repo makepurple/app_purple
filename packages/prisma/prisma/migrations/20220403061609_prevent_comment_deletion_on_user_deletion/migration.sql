fc0b4d9386ad7b659ac371b038f6465be8f89699
leedavidcs <lee.david.cs@gmail.com>
2022-04-02 23:19:09 -0700

    Prevent deletion of comments ever

---
+++ packages/prisma/prisma/migrations/20220403061609_prevent_comment_deletion_on_user_deletion/migration.sql
@@ -0,0 +1,14 @@
+-- DropForeignKey
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_parentId_fkey";
+
+-- AlterTable
+ALTER TABLE "Comment" ALTER COLUMN "authorId" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Comment"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
