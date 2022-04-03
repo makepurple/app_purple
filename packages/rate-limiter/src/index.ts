ebec56f8acaee8d3f750718131d9fa3b4ee26318
leedavidcs <lee.david.cs@gmail.com>
2022-04-02 19:52:07 -0700

    Update rate-limiter to support upstash or ioredis

--- packages/rate-limiter/src/index.ts
+++ packages/rate-limiter/src/index.ts
@@ -1,7 +1,11 @@
-import { Redis } from "ioredis";
+import { Redis as URedis } from "@upstash/redis";
+import { Redis as IORedis } from "ioredis";
 
 export interface RateLimiterOptions {
-	db: Redis;
+	db: {
+		ioredis?: IORedis;
+		upstash?: URedis;
+	};
 	durationMs: number;
 	max: number;
 	namespace?: string;
@@ -17,14 +21,37 @@ export interface RateLimiterStatus {
 	reset: number;
 }
 
+interface GetFromStoreParams {
+	key: string;
+	nowMs: number;
+	startMs: number;
+}
+
+interface GetFromStoreResult {
+	count: number;
+	reset: number;
+}
+
 export class RateLimiter {
-	private db: Redis;
+	private ioredis?: IORedis;
+	private upstash?: URedis;
 	private durationMs: number;
 	private max: number;
 	private namespace: string;
 
 	constructor(options: RateLimiterOptions) {
-		this.db = options.db;
+		if (options.db.upstash && options.db.ioredis) {
+			throw new Error("Must provide only 1 of either upstash or ioredis!");
+		}
+
+		if (options.db.upstash) {
+			this.upstash = options.db.upstash;
+		}
+
+		if (options.db.ioredis) {
+			this.ioredis = options.db.ioredis;
+		}
+
 		this.durationMs = options.durationMs;
 		this.max = options.max;
 		this.namespace = options.namespace ?? "limit";
@@ -39,23 +66,17 @@ export class RateLimiter {
 		const nowMs = performance.now();
 		const startMs = nowMs - this.durationMs;
 
-		const res = (await this.db
-			.multi()
-			.zremrangebyscore(key, 0, startMs)
-			.zcard(key)
-			.zadd(key, nowMs, nowMs)
-			.zrange(key, 0, 0)
-			.zrange(key, -this.max, -this.max)
-			.zremrangebyrank(key, 0, -(this.max + 1))
-			.pexpire(key, this.durationMs)
-			.exec()) as readonly [error: Error, result: string][];
+		const getParams: GetFromStoreParams = { key, nowMs, startMs };
 
-		const durationSecs = this.durationMs / 1_000;
+		const dbResults = this.upstash
+			? await this.getWithUpstash(getParams)
+			: this.ioredis
+			? await this.getWithIORedis(getParams)
+			: null;
 
-		const count = parseInt(res[1][1]);
-		const oldest = parseInt(res[3][1]);
-		const oldestInRange = parseInt(res[4][1]);
-		const reset = (Number.isNaN(oldestInRange) ? oldest : oldestInRange) + durationSecs;
+		if (!dbResults) throw new Error();
+
+		const { count, reset } = dbResults;
 
 		return {
 			// X-Rate-Limit-Limit
@@ -68,5 +89,97 @@ export class RateLimiter {
 			// The time at which the rate-limit resets
 			reset: Math.floor(reset / 1_000)
 		};
+	}
+
+	private toNumber(value: Maybe<string | number>): number | null {
+		if (value === null) return null;
+
+		if (typeof value === "undefined") return null;
+		if (typeof value === "number") return value;
+
+		const parsed = parseInt(value, 10);
+
+		if (Number.isNaN(parsed)) return null;
+
+		return parsed;
+	}
+
+	private async getWithUpstash(params: GetFromStoreParams): Promise<GetFromStoreResult> {
+		const { key, nowMs, startMs } = params;
+
+		if (!this.upstash) throw new Error("Error rate-limiting with upstash!");
+
+		const res = await this.upstash
+			.pipeline()
+			.zremrangebyscore(key, 0, startMs)
+			.zcard(key)
+			.zadd(key, {
+				score: nowMs,
+				member: `${nowMs}`
+			})
+			.zrange(key, 0, 0)
+			.zrange(key, -this.max, -this.max)
+			.zremrangebyrank(key, 0, -(this.max + 1))
+			.pexpire(key, this.durationMs)
+			.exec<Maybe<{ result?: string | number | undefined }>[]>()
+			.then((response) => response?.map((item) => item?.result ?? null) ?? []);
+
+		const durationSecs = this.durationMs / 1_000;
+
+		const count = this.toNumber(res[1]);
+		const oldest = this.toNumber(res[3]);
+		const oldestInRange = this.toNumber(res[4]);
+
+		const finalOldest = oldestInRange ?? oldest;
+
+		if (typeof count !== "number") throw new Error("Could not get rate-limit remaining");
+		if (typeof finalOldest !== "number") throw new Error("Could not get rate-limit reset");
+
+		const reset = finalOldest + durationSecs;
+
+		return { count, reset };
+	}
+
+	private async getWithIORedis(params: GetFromStoreParams): Promise<GetFromStoreResult> {
+		const { key, nowMs, startMs } = params;
+
+		if (!this.ioredis) throw new Error("Error rate-limiting with ioredis!");
+
+		const res = await this.ioredis
+			.multi()
+			.zremrangebyscore(key, 0, startMs)
+			.zcard(key)
+			.zadd(key, nowMs, `${nowMs}`)
+			.zrange(key, 0, 0)
+			.zrange(key, -this.max, -this.max)
+			.zremrangebyrank(key, 0, -(this.max + 1))
+			.pexpire(key, this.durationMs)
+			.exec()
+			.then((response) => {
+				if (!response) return [];
+
+				return response.map((item) => {
+					if (!item) return null;
+
+					const [, result] = item;
+
+					return (result as Maybe<string | number>) ?? null;
+				});
+			});
+
+		const durationSecs = this.durationMs / 1_000;
+
+		const count = this.toNumber(res[1]);
+		const oldest = this.toNumber(res[3]);
+		const oldestInRange = this.toNumber(res[4]);
+
+		const finalOldest = oldestInRange ?? oldest;
+
+		if (typeof count !== "number") throw new Error("Could not get rate-limit remaining");
+		if (typeof finalOldest !== "number") throw new Error("Could not get rate-limit reset");
+
+		const reset = finalOldest + durationSecs;
+
+		return { count, reset };
 	}
 }
