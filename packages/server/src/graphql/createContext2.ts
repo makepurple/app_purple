d931b5dd3c590b16daf1f731da0940daff75c4e4
leedavidcs <lee.david.cs@gmail.com>
2022-04-10 23:46:20 -0700

    Add context props

--- packages/server/src/graphql/createContext2.ts
+++ packages/server/src/graphql/createContext2.ts
@@ -1,15 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getToken } from "next-auth/jwt";
+import * as octokit from "../services/octokit";
 
 export interface CreateContextParams {
 	req: NextApiRequest;
 	res: NextApiResponse;
 }
 
-export const createContext2 = (params: CreateContextParams) => {
+export const createContext2 = async (params: CreateContextParams): Promise<any> => {
 	const { req, res } = params;
 
+	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+	const jwt = await getToken({ req, secret: process.env.NEXTAUTH_SECRET! });
+
 	return {
+		jwt,
+		octokit: await octokit.client.graphql(jwt?.accessToken),
 		req,
-		res
+		res,
+		user: jwt && {
+			id: jwt.sub,
+			name: jwt.name,
+			email: jwt.email
+		}
 	};
 };
