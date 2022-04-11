d7efce0072dcf809acf0a11a4f6861f28db16888
leedavidcs <lee.david.cs@gmail.com>
2022-04-10 23:28:17 -0700

    Minimize context

--- packages/server/src/graphql/createContext2.ts
+++ packages/server/src/graphql/createContext2.ts
@@ -1,25 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getToken } from "next-auth/jwt";
 
 export interface CreateContextParams {
 	req: NextApiRequest;
 	res: NextApiResponse;
 }
 
-export const createContext2 = async (params: CreateContextParams): Promise<any> => {
+export const createContext2 = (params: CreateContextParams) => {
 	const { req, res } = params;
 
-	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-	const jwt = await getToken({ req, secret: process.env.NEXTAUTH_SECRET! });
-
 	return {
-		jwt,
 		req,
-		res,
-		user: jwt && {
-			id: jwt.sub,
-			name: jwt.name,
-			email: jwt.email
-		}
+		res
 	};
 };
