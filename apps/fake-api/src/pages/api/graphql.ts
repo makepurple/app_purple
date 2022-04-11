6f1a5113bcc7adae869a45676ba0cd2b038a87f0
leedavidcs <lee.david.cs@gmail.com>
2022-04-10 22:59:25 -0700

    Mirror more graphql endpoint

--- apps/fake-api/src/pages/api/graphql.ts
+++ apps/fake-api/src/pages/api/graphql.ts
@@ -1,6 +1,10 @@
 import { ApolloServer } from "apollo-server-micro";
 import { GraphQLBoolean, GraphQLObjectType, GraphQLSchema } from "graphql";
+import { processRequest } from "graphql-upload";
+import Cors from "micro-cors";
 import { NextApiRequest, NextApiResponse } from "next";
+
+const cors = Cors();
 
 const schema = new GraphQLSchema({
 	query: new GraphQLObjectType({
@@ -24,12 +28,32 @@ export const config = {
 	api: { bodyParser: false }
 };
 
-export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+export default cors(async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+	if (req.method === "OPTIONS") {
+		res.end();
+
+		return;
+	}
+
 	await startServer;
+
+	/**
+	 * !HACK
+	 * @description Get file uploads to work with ApolloServer 3.0 using
+	 * graphql-upload
+	 * @see (@link: https://github.com/jaydenseric/graphql-upload/issues/160#issuecomment-822729490)
+	 * @author David Lee
+	 * @date December 19, 2021
+	 */
+	const contentType = req.headers["content-type"];
+
+	if (contentType?.startsWith("multipart/form-data")) {
+		(req as any).filePayload = await processRequest(req, res);
+	}
 
 	const graphqlHandler = server.createHandler({
 		path: "/api/graphql"
 	});
 
 	await graphqlHandler(req, res);
-};
+});
