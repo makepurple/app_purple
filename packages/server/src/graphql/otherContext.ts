514b53a885b1d752f4e43bbfce7a6ac227febaa0
leedavidcs <lee.david.cs@gmail.com>
2022-04-11 19:29:48 -0700

    Remove everything except prisma and jwt

--- packages/server/src/graphql/otherContext.ts
+++ packages/server/src/graphql/otherContext.ts
@@ -1,11 +1,6 @@
-import isbot from "isbot";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getToken } from "next-auth/jwt";
-import { getClientIp } from "request-ip";
 import { prisma } from "../db";
-import { redis } from "../redis";
-import { CloudinaryClient } from "../services/cloudinary";
-import * as pusher from "../services/pusher";
 
 export const makeContext = async (params: {
 	req: NextApiRequest;
@@ -17,12 +12,8 @@ export const makeContext = async (params: {
 	const jwt = await getToken({ req, secret: process.env.NEXTAUTH_SECRET! });
 
 	return {
-		cloudinary: new CloudinaryClient(),
-		ip: isbot(req.headers["user-agent"]) ? null : getClientIp(req),
 		jwt,
 		prisma,
-		pusher: pusher.client,
-		redis,
 		req,
 		res,
 		user: jwt && {
