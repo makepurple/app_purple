7463db77d36498c28be874b905e4da5abf2ea313
leedavidcs <lee.david.cs@gmail.com>
2022-04-11 19:39:24 -0700

    Re-add graphql api

--- apps/fake-api/src/pages/api/graphql.ts
+++ apps/fake-api/src/pages/api/graphql.ts
@@ -1,39 +1,31 @@
-// import { createServer } from "@graphql-yoga/node";
-// import { makeContext } from "@makepurple/server/graphql/otherContext";
-// import { GraphQLBoolean, GraphQLObjectType, GraphQLSchema } from "graphql";
+import { createServer } from "@graphql-yoga/node";
+import { makeContext } from "@makepurple/server/graphql/otherContext";
+import { GraphQLBoolean, GraphQLObjectType, GraphQLSchema } from "graphql";
 
-import { NextApiRequest, NextApiResponse } from "next";
+const schema = new GraphQLSchema({
+	query: new GraphQLObjectType({
+		name: "Query",
+		fields: {
+			ok: {
+				type: GraphQLBoolean,
+				resolve: () => true
+			}
+		}
+	})
+});
 
-// const schema = new GraphQLSchema({
-// 	query: new GraphQLObjectType({
-// 		name: "Query",
-// 		fields: {
-// 			ok: {
-// 				type: GraphQLBoolean,
-// 				resolve: () => true
-// 			}
-// 		}
-// 	})
-// });
+const server = createServer({
+	context: makeContext,
+	schema,
+	cors: false,
+	endpoint: "/api/graphql"
+});
 
-// const server = createServer({
-// 	context: makeContext,
-// 	schema,
-// 	cors: false,
-// 	endpoint: "/api/graphql"
-// });
-
-// export const config = {
-// 	api: {
-// 		bodyParser: false,
-// 		externalResolver: true
-// 	}
-// };
-
-// export default server;
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-	return res.json({
-		message: "hello world~!"
-	});
+export const config = {
+	api: {
+		bodyParser: false,
+		externalResolver: true
+	}
 };
+
+export default server;
