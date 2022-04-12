1ee8de3ecd073dfdc9ebfb1b3af8fd196fb000f4
leedavidcs <lee.david.cs@gmail.com>
2022-04-12 02:07:35 -0700

    Add all schema stuff

--- packages/server/src/graphql/nexus/schema.ts
+++ packages/server/src/graphql/nexus/schema.ts
@@ -3,6 +3,8 @@ import { lexicographicSortSchema } from "graphql";
 import { fieldAuthorizePlugin, makeSchema } from "nexus";
 import path from "path";
 import * as inputTypes from "./input-types";
+import * as mutations from "./mutations";
+import * as queries from "./queries";
 import * as types from "./types";
 
 const isGenerateScript: boolean = process.argv.includes("--nexus-exit");
@@ -10,7 +12,7 @@ const isGenerateScript: boolean = process.argv.includes("--nexus-exit");
 const _schema = makeSchema({
 	shouldGenerateArtifacts: isGenerateScript,
 	shouldExitAfterGenerateArtifacts: isGenerateScript,
-	types: { ...inputTypes, ...types },
+	types: { ...inputTypes, ...mutations, ...queries, ...types },
 	outputs: {
 		schema: path.resolve(__dirname, "./generated/schema.gen.graphql"),
 		typegen: path.resolve(__dirname, "./generated/typegen.gen.d.ts")
