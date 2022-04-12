4f3a3a0185eee5fe0bd6120f779235feaa733ae7
leedavidcs <lee.david.cs@gmail.com>
2022-04-12 00:07:46 -0700

    Add input-types. Remove object types

--- packages/server/src/graphql/nexus/schema.ts
+++ packages/server/src/graphql/nexus/schema.ts
@@ -2,14 +2,15 @@ import { stripIndents } from "common-tags";
 import { lexicographicSortSchema } from "graphql";
 import { fieldAuthorizePlugin, makeSchema } from "nexus";
 import path from "path";
-import * as types from "./types";
+import * as inputTypes from "./input-types";
+import { Query } from "./types/Query";
 
 const isGenerateScript: boolean = process.argv.includes("--nexus-exit");
 
 const _schema = makeSchema({
 	shouldGenerateArtifacts: isGenerateScript,
 	shouldExitAfterGenerateArtifacts: isGenerateScript,
-	types: { ...types },
+	types: { ...inputTypes, Query },
 	outputs: {
 		schema: path.resolve(__dirname, "./generated/schema.gen.graphql"),
 		typegen: path.resolve(__dirname, "./generated/typegen.gen.d.ts")
