97fc2ff3729df31e874947abb26d666e2b5a162f
leedavidcs <lee.david.cs@gmail.com>
2022-04-12 01:39:51 -0700

    Don't use Winston namespace

--- packages/server/src/utils/logger.util.ts
+++ packages/server/src/utils/logger.util.ts
@@ -1,6 +1,6 @@
-import Winston, { createLogger, transports } from "winston";
+import { createLogger, transports } from "winston";
 
-export const Logger: Winston.Logger = createLogger({
+export const Logger = createLogger({
 	silent: process.env.NODE_ENV !== "development",
 	transports: [new transports.Console()]
 });
