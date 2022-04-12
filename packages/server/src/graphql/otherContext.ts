0ef895d2fff6bc779ab8f53e306032b8d578e23c
leedavidcs <lee.david.cs@gmail.com>
2022-04-11 19:59:53 -0700

    Re-add most context stuff

--- packages/server/src/graphql/otherContext.ts
+++ packages/server/src/graphql/otherContext.ts
@@ -1,5 +1,11 @@
+import isbot from "isbot";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getToken } from "next-auth/jwt";
+import { getClientIp } from "request-ip";
 import { prisma } from "../db";
+import { redis } from "../redis";
+import { CloudinaryClient } from "../services/cloudinary";
+import * as pusher from "../services/pusher";
 
 export const makeContext = async (params: {
 	req: NextApiRequest;
@@ -7,9 +13,22 @@ export const makeContext = async (params: {
 }): Promise<any> => {
 	const { req, res } = params;
 
-	return await Promise.resolve({
+	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+	const jwt = await getToken({ req, secret: process.env.NEXTAUTH_SECRET! });
+
+	return {
+		cloudinary: new CloudinaryClient(),
+		ip: isbot(req.headers["user-agent"]) ? null : getClientIp(req),
+		jwt,
 		prisma,
+		pusher: pusher.client,
+		redis,
 		req,
-		res
-	});
+		res,
+		user: jwt && {
+			id: jwt.sub,
+			name: jwt.name,
+			email: jwt.email
+		}
+	};
 };
