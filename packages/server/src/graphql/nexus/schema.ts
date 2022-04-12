4602ffb34e94e4010ff968a0f1fa66e281fb2b61
leedavidcs <lee.david.cs@gmail.com>
2022-04-11 23:55:22 -0700

    Only import type Query

--- packages/server/src/graphql/nexus/schema.ts
+++ packages/server/src/graphql/nexus/schema.ts
@@ -2,15 +2,14 @@ import { stripIndents } from "common-tags";
 import { lexicographicSortSchema } from "graphql";
 import { fieldAuthorizePlugin, makeSchema } from "nexus";
 import path from "path";
-import * as inputTypes from "./input-types";
-import * as types from "./types";
+import { Query } from "./types/Query";
 
 const isGenerateScript: boolean = process.argv.includes("--nexus-exit");
 
 const _schema = makeSchema({
 	shouldGenerateArtifacts: isGenerateScript,
 	shouldExitAfterGenerateArtifacts: isGenerateScript,
-	types: { ...inputTypes, ...types },
+	types: { Query },
 	outputs: {
 		schema: path.resolve(__dirname, "./generated/schema.gen.graphql"),
 		typegen: path.resolve(__dirname, "./generated/typegen.gen.d.ts")
