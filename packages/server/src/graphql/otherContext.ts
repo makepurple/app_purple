4fb84415f44e6d1565ab47fe6c8c00f32e44cad9
leedavidcs <lee.david.cs@gmail.com>
2022-04-11 19:32:12 -0700

    Remove all but prisma

--- packages/server/src/graphql/otherContext.ts
+++ packages/server/src/graphql/otherContext.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getToken } from "next-auth/jwt";
 import { prisma } from "../db";
 
 export const makeContext = async (params: {
@@ -8,18 +7,9 @@ export const makeContext = async (params: {
 }): Promise<any> => {
 	const { req, res } = params;
 
-	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-	const jwt = await getToken({ req, secret: process.env.NEXTAUTH_SECRET! });
-
-	return {
-		jwt,
+	return await Promise.resolve({
 		prisma,
 		req,
-		res,
-		user: jwt && {
-			id: jwt.sub,
-			name: jwt.name,
-			email: jwt.email
-		}
-	};
+		res
+	});
 };
