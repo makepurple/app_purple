d75ec92dd218dea3ec2710b0ef04c6f041095fcb
leedavidcs <lee.david.cs@gmail.com>
2022-04-11 21:47:36 -0700

    Slim other ctx to prisma

--- packages/server/src/graphql/otherContext.ts
+++ packages/server/src/graphql/otherContext.ts
@@ -1,37 +1,15 @@
-import isbot from "isbot";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getToken } from "next-auth/jwt";
-import { getClientIp } from "request-ip";
 import { prisma } from "../db";
-import { redis } from "../redis";
-import { CloudinaryClient } from "../services/cloudinary";
-import * as octokit from "../services/octokit";
-import * as pusher from "../services/pusher";
-import type { ServerContext } from "./context";
 
 export const makeContext = async (params: {
 	req: NextApiRequest;
 	res: NextApiResponse;
-}): Promise<ServerContext> => {
+}): Promise<any> => {
 	const { req, res } = params;
 
-	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-	const jwt = await getToken({ req, secret: process.env.NEXTAUTH_SECRET! });
-
-	return {
-		cloudinary: new CloudinaryClient(),
-		ip: isbot(req.headers["user-agent"]) ? null : getClientIp(req),
-		jwt,
-		octokit: await octokit.client.graphql(jwt?.accessToken),
+	return Promise.resolve({
 		prisma,
-		pusher: pusher.client,
-		redis,
 		req,
-		res,
-		user: jwt && {
-			id: jwt.sub,
-			name: jwt.name,
-			email: jwt.email
-		}
-	};
+		res
+	});
 };
