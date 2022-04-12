25f34a0a1dfa38eade9f08b1f6c4117e07c5ea80
leedavidcs <lee.david.cs@gmail.com>
2022-04-12 01:47:05 -0700

    Just use console.log directly

--- packages/server/src/utils/logger.util.ts
+++ packages/server/src/utils/logger.util.ts
@@ -1,6 +1,13 @@
-import { createLogger, transports } from "winston";
-
-export const Logger = createLogger({
-	silent: process.env.NODE_ENV !== "development",
-	transports: [new transports.Console()]
-});
+/* eslint-disable no-console */
+export const Logger = {
+	error: (...args: any[]) => {
+		if (process.env.NODE_ENV !== "production") {
+			console.log(...args);
+		}
+	},
+	info: (...args: any[]) => {
+		if (process.env.NODE_ENV !== "production") {
+			console.log(...args);
+		}
+	}
+};
