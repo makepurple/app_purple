5e95e169f898a355e9ad34375e71db13c6878a39
leedavidcs <lee.david.cs@gmail.com>
2022-04-11 21:25:36 -0700

    Use otherContext again, with mirrored makeContext

--- apps/fake-api/src/pages/api/graphql.ts
+++ apps/fake-api/src/pages/api/graphql.ts
@@ -1,5 +1,5 @@
 import { createServer } from "@graphql-yoga/node";
-import { makeContext } from "@makepurple/server/graphql/makeContext";
+import { makeContext } from "@makepurple/server/graphql/otherContext";
 import { GraphQLBoolean, GraphQLObjectType, GraphQLSchema } from "graphql";
 
 const schema = new GraphQLSchema({
