61f13e3169b3f213f9c1c2a3bf25dae8ee980e3f
leedavidcs <lee.david.cs@gmail.com>
2022-04-12 23:18:27 -0700

    Remove cors

--- apps/www/src/pages/api/graphql.ts
+++ apps/www/src/pages/api/graphql.ts
@@ -1,9 +1,6 @@
 import { getApolloServer, makeContext, schema } from "@makepurple/server";
 import { processRequest } from "graphql-upload";
-import Cors from "micro-cors";
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
-
-const cors = Cors();
 
 export const config = {
 	api: {
@@ -19,36 +16,34 @@ const server = getApolloServer({
 
 const startServer = server.start();
 
-const handler: NextApiHandler = cors(
-	async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-		if (req.method === "OPTIONS") {
-			res.end();
+const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+	if (req.method === "OPTIONS") {
+		res.end();
 
-			return;
-		}
-
-		await startServer;
-
-		/**
-		 * !HACK
-		 * @description Get file uploads to work with ApolloServer 3.0 using
-		 * graphql-upload
-		 * @see (@link: https://github.com/jaydenseric/graphql-upload/issues/160#issuecomment-822729490)
-		 * @author David Lee
-		 * @date December 19, 2021
-		 */
-		const contentType = req.headers["content-type"];
-
-		if (contentType?.startsWith("multipart/form-data")) {
-			(req as any).filePayload = await processRequest(req, res);
-		}
-
-		const graphqlHandler = server.createHandler({
-			path: "/api/graphql"
-		});
-
-		await graphqlHandler(req, res);
+		return;
 	}
-);
+
+	await startServer;
+
+	/**
+	 * !HACK
+	 * @description Get file uploads to work with ApolloServer 3.0 using
+	 * graphql-upload
+	 * @see (@link: https://github.com/jaydenseric/graphql-upload/issues/160#issuecomment-822729490)
+	 * @author David Lee
+	 * @date December 19, 2021
+	 */
+	const contentType = req.headers["content-type"];
+
+	if (contentType?.startsWith("multipart/form-data")) {
+		(req as any).filePayload = await processRequest(req, res);
+	}
+
+	const graphqlHandler = server.createHandler({
+		path: "/api/graphql"
+	});
+
+	await graphqlHandler(req, res);
+};
 
 export default handler;
