e0fa2b96d7fedcf64cb4921b309a4482df118eca
leedavidcs <lee.david.cs@gmail.com>
2022-04-12 23:53:34 -0700

    Delete fake-api. Delete some www dependencies

--- apps/fake-api/src/pages/api/hello.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-	return res.json({
-		message: "hello world~!"
-	});
-};
--- apps/fake-api/types.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="@makepurple/typings" />
-/// <reference types="@makepurple/server" />
