216c2db6398a7a1203b56c5235a5e87c65138baa
leedavidcs <lee.david.cs@gmail.com>
2022-04-17 01:00:04 -0700

    Delete unused hello route

--- apps/www/src/pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-	console.log("hello world~!");
-
-	return res.json({
-		message: "hello world~!"
-	});
-};
