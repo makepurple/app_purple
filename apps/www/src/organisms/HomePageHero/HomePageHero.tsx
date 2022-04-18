20287b56a2edfce137a20f1ef12ecf7683a9fb19
leedavidcs <lee.david.cs@gmail.com>
2022-04-17 22:07:51 -0700

    Move HomePageHero backgrounds to main component

--- apps/www/src/organisms/HomePageHero/HomePageHero.tsx
+++ apps/www/src/organisms/HomePageHero/HomePageHero.tsx
@@ -1,13 +1,56 @@
 import { Button } from "@makepurple/components";
+import { oneLine } from "common-tags";
 import NextLink from "next/link";
 import React, { CSSProperties, FC } from "react";
-import tw from "twin.macro";
+import tw, { styled, theme } from "twin.macro";
 import { TypistLogo } from "../TypistLogo";
 
 const Root = tw.div`
+	relative
 	flex
 	flex-col
 	items-center
+	justify-center
+	w-full
+	min-h-screen
+	py-24
+`;
+
+const GradientBackground = styled.div`
+	${tw`
+		absolute
+		inset-0
+		opacity-[14%]
+	`}
+	background: ${oneLine`
+		linear-gradient(
+			-80deg,
+			${theme`colors.pink.600`},
+			${theme`colors.violet.600`},
+			${theme`colors.blue.500`})
+	`};
+`;
+
+const GridBackground = tw.div`
+	absolute
+	inset-0
+	background-image[linear-gradient(#6366f1 1.3px, transparent 1.3px), linear-gradient(to right, #6366f1 1.3px, #f8fafc 1.3px)]
+	bg-[#f8fafc]
+	opacity-10
+	background-size[1.5rem 1.5rem]
+`;
+
+const RadialBackground = tw.div`
+	absolute
+	inset-0
+	background-image[radial-gradient(circle, #f8fafc 35%, #6366f100 75%)]
+`;
+
+const Contents = tw.div`
+	flex
+	flex-col
+	items-center
+	z-[1]
 `;
 
 const TagLine = tw.h1`
@@ -47,19 +90,24 @@ export interface HomePageHeroProps {
 export const HomePageHero: FC<HomePageHeroProps> = ({ className, style }) => {
 	return (
 		<Root className={className} style={style}>
-			<TagLine>
-				<Line>Discover</Line>
-				<Line>Developers</Line>
-				<Line>
-					for <TypistLogo sentences={["next.js", "urql", "prisma"]} />
-				</Line>
-			</TagLine>
-			<Info tw="mt-5 md:mt-10">Developer profiles powered-by GitHub discoverability</Info>
-			<NextLink href="/signup" passHref>
-				<SignUpButton as="a" size="large" type="button" tw="mt-5 md:mt-10">
-					<span>Get Started</span>
-				</SignUpButton>
-			</NextLink>
+			<GradientBackground />
+			<GridBackground />
+			<RadialBackground />
+			<Contents>
+				<TagLine>
+					<Line>Discover</Line>
+					<Line>Developers</Line>
+					<Line>
+						for <TypistLogo sentences={["next.js", "urql", "prisma"]} />
+					</Line>
+				</TagLine>
+				<Info tw="mt-5 md:mt-10">Developer profiles powered-by GitHub discoverability</Info>
+				<NextLink href="/signup" passHref>
+					<SignUpButton as="a" size="large" type="button" tw="mt-5 md:mt-10">
+						<span>Get Started</span>
+					</SignUpButton>
+				</NextLink>
+			</Contents>
 		</Root>
 	);
 };
--- apps/www/src/pages/index.tsx
+++ apps/www/src/pages/index.tsx
@@ -1,7 +1,6 @@
-import { oneLine } from "common-tags";
 import { NextPage } from "next";
 import React from "react";
-import tw, { styled, theme } from "twin.macro";
+import tw from "twin.macro";
 import { HomePageHero } from "../organisms";
 import { pageProps, PageProps } from "../page-props";
 
@@ -13,62 +12,12 @@ const Root = tw.div`
 	text-center
 `;
 
-const HeroContainer = tw.div`
-	relative
-	flex
-	flex-col
-	items-center
-	justify-center
-	w-full
-	min-h-screen
-	py-24
-`;
-
-const GradientBackground = styled.div`
-	${tw`
-		absolute
-		inset-0
-		opacity-[14%]
-	`}
-	background: ${oneLine`
-		linear-gradient(
-			-80deg,
-			${theme`colors.pink.600`},
-			${theme`colors.violet.600`},
-			${theme`colors.blue.500`})
-	`};
-`;
-
-const GridBackground = tw.div`
-	absolute
-	inset-0
-	background-image[linear-gradient(#6366f1 1.3px, transparent 1.3px), linear-gradient(to right, #6366f1 1.3px, #f8fafc 1.3px)]
-	bg-[#f8fafc]
-	opacity-10
-	background-size[1.5rem 1.5rem]
-`;
-
-const RadialBackground = tw.div`
-	absolute
-	inset-0
-	background-image[radial-gradient(circle, #f8fafc 35%, #6366f100 75%)]
-`;
-
-const Hero = tw(HomePageHero)`
-	z-[1]
-`;
-
 export const getStaticProps = pageProps;
 
 export const Page: NextPage<PageProps> = () => {
 	return (
 		<Root>
-			<HeroContainer>
-				<GradientBackground />
-				<GridBackground />
-				<RadialBackground />
-				<Hero />
-			</HeroContainer>
+			<HomePageHero />
 		</Root>
 	);
 };
