07208a152da7b740a8558606d0b961727b3bf1f2
leedavidcs <lee.david.cs@gmail.com>
2022-04-19 01:54:29 -0700

    Fix thumbnails

--- apps/www/src/organisms/HomePageProfileSection/HomePageProfileSection.tsx
+++ apps/www/src/organisms/HomePageProfileSection/HomePageProfileSection.tsx
@@ -3,6 +3,8 @@ import { dayjs } from "@makepurple/utils";
 import { codeBlock, oneLine } from "common-tags";
 import React, { CSSProperties, FC } from "react";
 import tw, { styled, theme } from "twin.macro";
+import post1Thumbnail from "../../../public/static/jpegs/demo-post-thumbnail.jpg";
+import post2Thumbnail from "../../../public/static/pngs/demo-post-2-thumbnail.png";
 import { PostCard } from "../PostCard";
 
 const post1Skills = ["next.js", "prisma", "nexus", "urql", "TypeScript", "pnpm", "turborepo"].map(
@@ -199,7 +201,7 @@ export const HomePageProfileSection: FC<HomePageProfileSectionProps> = ({ classN
 							})),
 							nodes: post1Skills
 						},
-						thumbnailUrl: "/static/pngs/demo-post-thumbnail.png",
+						thumbnailUrl: post1Thumbnail as any,
 						title: "How MakePurple Works",
 						upvotes: 12,
 						urlSlug: "posts"
@@ -231,7 +233,7 @@ export const HomePageProfileSection: FC<HomePageProfileSectionProps> = ({ classN
 							})),
 							nodes: post2Skills
 						},
-						thumbnailUrl: "/static/pngs/demo-post-2-thumbnail.png",
+						thumbnailUrl: post2Thumbnail as any,
 						title: "Consumer-First GraphQL API Design",
 						upvotes: 27,
 						urlSlug: "posts"
