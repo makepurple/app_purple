70dd56fff8ee5ac9f32e3e13b6774d25239cd413
leedavidcs <lee.david.cs@gmail.com>
2022-04-27 01:29:10 -0700

    Fix mock

--- apps/www/src/graphql/mocks/queries/GetMyUser.query.mock.ts
+++ apps/www/src/graphql/mocks/queries/GetMyUser.query.mock.ts
@@ -4,6 +4,7 @@ export const GetMyUser_mock: GetMyUserQuery = {
 	__typename: "Query",
 	viewer: {
 		__typename: "User" as const,
+		banned: false,
 		id: "0",
 		name: "DenverCoder9",
 		image: "https://avatars.githubusercontent.com/u/810438"
