41dd1be5a54ab53c2b1bd3576ddadc81f37c9d4b
leedavidcs <lee.david.cs@gmail.com>
2022-04-29 02:01:29 -0700

    Use content security headers

--- apps/www/src/pages/_middleware.ts
+++ apps/www/src/pages/_middleware.ts
@@ -8,15 +8,37 @@ const GDPR_COOKIE_MAX_AGE_SECONDS = ms("1y") / 1_000;
 const middleware: NextMiddleware = (req: NextRequest) => {
 	const { pathname } = req.nextUrl;
 
-	if (pathname.startsWith("/api")) return NextResponse.next();
-	if (pathname.startsWith("/banned")) return NextResponse.next();
+	const response = NextResponse.next();
+
+	const ContentSecurityPolicy = `
+		default-src 'self';
+		script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com *.cloudinary.com *.googletagmanager.com;
+		child-src *.youtube.com *.google.com *.twitter.com;
+		style-src 'self' 'unsafe-inline' *.googleapis.com;
+		img-src * blob: data:;
+		media-src 'none';
+		connect-src *;
+		font-src fonts.gstatic.com;
+	`;
+
+	response.headers.set("Content-Security-Policy", ContentSecurityPolicy.replace(/\n/g, ""));
+	response.headers.set("Referrer-Policy", "origin-when-cross-origin");
+	response.headers.set("Permissions-Policy", "camera=(), microphone=(), geolocation=()");
+	response.headers.set(
+		"Strict-Transport-Security",
+		"max-age=31536000; includeSubDomains; preload"
+	);
+	response.headers.set("X-Frame-Options", "DENY");
+	response.headers.set("X-Content-Type-Options", "nosniff");
+	response.headers.set("X-DNS-Prefetch-Control", "on");
+
+	if (pathname.startsWith("/api")) return response;
+	if (pathname.startsWith("/banned")) return response;
 
 	const country = req.geo?.country;
 	const acceptGdprConsent = !!country && !isEuMember(country);
 
-	if (!acceptGdprConsent) return NextResponse.next();
-
-	const response = NextResponse.next();
+	if (!acceptGdprConsent) return response;
 
 	/**
 	 * !HACK
