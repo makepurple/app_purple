a33f3108d0b4974443fdc7e8247acf6173596eff
leedavidcs <lee.david.cs@gmail.com>
2022-05-14 00:53:51 -0700

    Improve activityFeed results

--- packages/server/src/graphql/nexus/queries/activityFeed.ts
+++ packages/server/src/graphql/nexus/queries/activityFeed.ts
@@ -1,4 +1,4 @@
-import { UserActivityType } from "@prisma/client";
+import { Prisma, UserActivityType } from "@prisma/client";
 import { arg, intArg, nonNull, queryField, stringArg } from "nexus";
 import { PrismaUtils } from "../../../utils";
 
@@ -12,16 +12,11 @@ export const activityFeed = queryField("activityFeed", {
 		where: arg({ type: "UserActivityWhereInput" })
 	},
 	resolve: async (parent, args, { prisma, user }) => {
-		const connection = await PrismaUtils.findManyCursorConnection<any, { id: string }>(
-			(paginationArgs) =>
-				prisma.userActivity
-					.findMany({
-						...paginationArgs,
-						orderBy: {
-							updatedAt: "desc"
-						},
-						where: {
-							...PrismaUtils.nonNull(args.where),
+		const where: Prisma.UserActivityWhereInput = user
+			? {
+					OR: [
+						// Get only "high-value" activities for skill follows
+						{
 							AND: [
 								{
 									OR: [
@@ -43,41 +38,117 @@ export const activityFeed = queryField("activityFeed", {
 										}
 									]
 								},
-								...(user
-									? [
-											{
-												OR: [
-													{
-														skills: {
-															some: {
-																followedBy: {
-																	some: {
-																		follower: {
-																			id: {
-																				equals: user.id
-																			}
-																		}
-																	}
-																}
-															}
-														}
-													},
-													{
-														user: {
-															followedBy: {
-																some: {
-																	follower: {
-																		id: { equals: user.id }
-																	}
-																}
-															}
+								{
+									skills: {
+										some: {
+											followedBy: {
+												some: {
+													follower: {
+														id: {
+															equals: user.id
 														}
 													}
-												]
+												}
 											}
-									  ]
-									: [])
+										}
+									}
+								}
 							]
+						},
+						// Get all activitiese for user follows
+						{
+							AND: [
+								{
+									OR: [
+										{
+											type: UserActivityType.CommentCodeExample,
+											comment: {}
+										},
+										{
+											type: UserActivityType.CommentPost,
+											comment: {}
+										},
+										{
+											type: UserActivityType.CreateCodeExample,
+											codeExample: {}
+										},
+										{
+											type: UserActivityType.FollowSkill,
+											follow: {}
+										},
+										{
+											type: UserActivityType.FollowUser,
+											follow: {}
+										},
+										{
+											type: UserActivityType.FriendAcceptUser,
+											follow: {}
+										},
+										{
+											type: UserActivityType.Joined
+										},
+										{
+											type: UserActivityType.PublishPost,
+											post: {}
+										},
+										{
+											type: UserActivityType.UpvoteCodeExample,
+											codeExample: {}
+										},
+										{
+											type: UserActivityType.UpvotePost,
+											post: {}
+										}
+									]
+								},
+								{
+									user: {
+										followedBy: {
+											some: {
+												follower: {
+													id: { equals: user.id }
+												}
+											}
+										}
+									}
+								}
+							]
+						}
+					]
+			  }
+			: {
+					// Get only "high-value" activitiese for un-authed users
+					OR: [
+						{
+							type: UserActivityType.CreateCodeExample,
+							codeExample: {}
+						},
+						{
+							type: UserActivityType.FollowSkill,
+							follow: {}
+						},
+						{
+							type: UserActivityType.FollowUser,
+							follow: {}
+						},
+						{
+							type: UserActivityType.PublishPost,
+							post: {}
+						}
+					]
+			  };
+
+		const connection = await PrismaUtils.findManyCursorConnection<any, { id: string }>(
+			(paginationArgs) =>
+				prisma.userActivity
+					.findMany({
+						...paginationArgs,
+						orderBy: {
+							updatedAt: "desc"
+						},
+						where: {
+							...PrismaUtils.nonNull(args.where),
+							...where
 						}
 					})
 					.then((items) => {
@@ -90,17 +161,7 @@ export const activityFeed = queryField("activityFeed", {
 				prisma.userActivity.count({
 					where: {
 						...PrismaUtils.nonNull(args.where),
-						...(user
-							? {
-									user: {
-										followedBy: {
-											some: {
-												follower: { id: { equals: user.id } }
-											}
-										}
-									}
-							  }
-							: {})
+						...where
 					}
 				}),
 			{ ...PrismaUtils.handleRelayConnectionArgs(args) },
