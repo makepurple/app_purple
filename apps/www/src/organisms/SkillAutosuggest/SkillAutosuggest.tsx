da27849b53dbe44a16f577901db90dfaaeb71481
leedavidcs <lee.david.cs@gmail.com>
2022-05-24 18:54:47 -0700

    Widen valid input for skill autosuggest

--- apps/www/src/organisms/SkillAutosuggest/SkillAutosuggest.tsx
+++ apps/www/src/organisms/SkillAutosuggest/SkillAutosuggest.tsx
@@ -148,7 +148,7 @@ export const SkillAutosuggest = forwardRef<
 	});
 
 	const isInputValid = useMemo(
-		() => /^[a-z0-9.]+\/[a-z0-9.]+$/i.test(combobox.inputValue),
+		() => /^[a-z0-9.\-_]+\/[a-z0-9.\-_]+$/i.test(combobox.inputValue),
 		[combobox.inputValue]
 	);
 
